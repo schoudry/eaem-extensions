ca8c3e81484f4b39fe23b48adfe0d675474f6b9f
sreek <nalabotu@adobe.com>
2018-09-08 13:28:55 -0500

    deep links

--- eaem-64-extensions/eaem-touchui-extend-rte-select-deep-link/jcr_root/apps/eaem-touchui-extend-rte-select-deep-link/clientlib/deep-links.js
+++ eaem-64-extensions/eaem-touchui-extend-rte-select-deep-link/jcr_root/apps/eaem-touchui-extend-rte-select-deep-link/clientlib/deep-links.js
@@ -19,22 +19,103 @@
         initialize: function(config) {
             this.superClass.initialize.call(this, config);
 
+            var baseDialog = this;
+
             this.$rteDialog = this.$container.find("[data-rte-dialog=link]");
 
             var $path = this.$rteDialog.find(".rte-dialog-columnContainer:first");
 
-            $(getAnchorsSelect()).insertAfter($path);
+            $(baseDialog.getAnchorsSelect()).insertAfter($path);
 
-            function getAnchorsSelect(){
-                var html = '<div class="rte-dialog-columnContainer">' +
-                                '<div class="rte-dialog-column">' +
-                                    '<coral-select disabled="disabled" class="coral-Form-field" placeholder="Select Link" id="' + EAEM_ANCHOR_SELECT + '">' +
-                                    '</coral-select>' +
-                                '</div>' +
-                           '</div>';
+            $path.find("foundation-autocomplete").on("foundation-field-change", constructSelect);
 
-                return html;
+            function constructSelect() {
+                var pathField = $(this).adaptTo('foundation-field');
+
+                $.get(pathField.getValue() + ".html?wcmmode=disabled").done(successFn);
+
+                function successFn(respHtml){
+                    var $aTags = $(respHtml).find("a"), options = [];
+
+                    $aTags.each(function( i, aTag ) {
+                        if(_.isEmpty(aTag.name)){
+                            return;
+                        }
+
+                        options.push(aTag.name);
+                    });
+
+                    $(baseDialog.getAnchorsSelect(options)).insertAfter($path);
+                }
             }
+        },
+
+        getAnchorsSelect: function(options, dValue){
+            $("#" + EAEM_ANCHOR_SELECT).remove();
+
+            var html = '<div class="rte-dialog-columnContainer" id="' + EAEM_ANCHOR_SELECT + '">' +
+                            '<div class="rte-dialog-column">' +
+                                '<coral-select class="coral-Form-field" placeholder="Select Link">';
+
+            _.each(options, function(option){
+                html = html + '<coral-select-item value="' + option + '"'
+                            + ( (dValue == option) ? " selected " : "" ) + '>' + option + '</coral-select-item>';
+            });
+
+            html = html + '</coral-select></div></div>';
+
+            return html;
+        },
+
+        dlgFromModel: function() {
+            if (_.isEmpty(this.objToEdit) || _.isEmpty(this.objToEdit.href)
+                        || (this.objToEdit.href.indexOf("#") == -1)) {
+                this.superClass.dlgFromModel.call(this);
+                return;
+            }
+
+            var href = this.objToEdit.href, anchor,
+                $path = this.$rteDialog.find(".rte-dialog-columnContainer:first");
+
+            this.objToEdit.href = href.substring(0, href.lastIndexOf("#"));
+
+            this.superClass.dlgFromModel.call(this);
+
+            anchor = href.substring(href.lastIndexOf("#") + 1);
+
+            $.get(href + ".html?wcmmode=disabled").done($.proxy(successFn, this));
+
+            function successFn(respHtml){
+                var $aTags = $(respHtml).find("a"), options = [];
+
+                $aTags.each(function( i, aTag ) {
+                    if(_.isEmpty(aTag.name)){
+                        return;
+                    }
+
+                    options.push(aTag.name);
+                });
+
+                $(this.getAnchorsSelect(options, anchor)).insertAfter($path);
+            }
+        },
+
+        dlgToModel: function() {
+            this.superClass.dlgToModel.call(this);
+
+            var anchorSelect = this.$dialog.find("#" + EAEM_ANCHOR_SELECT + " >* coral-select");
+
+            if(_.isEmpty(anchorSelect)){
+                return;
+            }
+
+            var aVal = anchorSelect.val();
+
+            if (_.isEmpty(aVal)) {
+                return;
+            }
+
+            this.objToEdit.href = this.objToEdit.href + "#" + aVal;
         }
     });
 
---
+++ eaem-64-extensions/eaem-touchui-pathfield-picker-show-large-preview/jcr_root/apps/eaem-touchui-pathfield-picker-show-large-preview/clientlib/show-preview.js
@@ -0,0 +1,103 @@
+(function ($, $document, gAuthor) {
+    if(!gAuthor){
+        return;
+    }
+
+    $document.on('dialog-ready', handlePathField);
+
+    function handlePathField(){
+        var $cuiPathBrowser = $(".coral-PathBrowser");
+
+        if(_.isEmpty($cuiPathBrowser)){
+            return;
+        }
+
+        //handle picker columns
+        $cuiPathBrowser.each(extendPicker);
+    }
+
+    function extendPicker(index, cuiPathBrowser){
+        cuiPathBrowser = $(cuiPathBrowser).data("pathBrowser");
+
+        var cuiPicker = cuiPathBrowser.$picker.data("picker");
+
+        cuiPathBrowser.$button.on("click", function() {
+            setTimeout(function(){
+                if(!cuiPicker.columnView){
+                    console.log("EAEM - could not initialize column view");
+                    return;
+                }
+
+                extendColumnView(cuiPicker.columnView);
+            }, 200);
+        });
+    }
+
+    function extendColumnView(columnView){
+        function handler(event, href, data){
+            if(_.isEmpty(href) || _.isEmpty(data)){
+                return;
+            }
+
+            var $item, assetPath, thumbHtml,
+                columnData = columnView._data[href];
+
+            if(_.isEmpty(columnData)){
+                return;
+            }
+
+            var $columnData = $(columnData), $items = $columnData.find(".coral-ColumnView-item");
+
+            $items.each(function(index, item){
+                $item = $(item);
+
+                assetPath = $item.data("value");
+
+                if(_.isEmpty(assetPath)){
+                    return;
+                }
+
+                thumbHtml = getThumbHtml(assetPath);
+
+                if(!thumbHtml){
+                    return;
+                }
+
+                $item.find(".coral-ColumnView-icon").html(thumbHtml);
+            });
+
+            columnView._data[href] = $columnData[0].outerHTML;
+        }
+
+        columnView.$element.on('coral-columnview-load', handler);
+    }
+
+    function getThumbHtml(assetPath){
+        var assetExt = getStringAfterLastDot(assetPath);
+
+        if(!assetExt){
+            return "";
+        }
+
+        return '<img class="foundation-collection-item-thumbnail" src="'
+                    + assetPath + '/_jcr_content/renditions/cq5dam.thumbnail.48.48.png" width="40px" height="40px" style="margin-right:0px" '
+                    + 'alt="' + getStringAfterLastSlash(assetPath) + '">';
+    }
+
+    function getStringAfterLastSlash(str){
+        if(!str || (str.indexOf("/") == -1)){
+            return "";
+        }
+
+        return str.substr(str.lastIndexOf("/") + 1);
+    }
+
+    function getStringAfterLastDot(str){
+        if(!str || (str.indexOf(".") == -1)){
+            return "";
+        }
+
+        return str.substr(str.lastIndexOf(".") + 1);
+    }
+
+}(jQuery, jQuery(document), Granite.author));
