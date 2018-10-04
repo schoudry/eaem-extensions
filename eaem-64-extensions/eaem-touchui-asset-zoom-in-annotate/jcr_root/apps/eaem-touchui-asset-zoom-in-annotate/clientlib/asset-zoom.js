d7c034e09bb403829475515452c1bef9ff9640ed
sreek <nalabotu@adobe.com>
2018-10-04 07:49:25 -0500

    zoom

--- eaem-64-extensions/eaem-touchui-asset-zoom-in-annotate/jcr_root/apps/eaem-touchui-asset-zoom-in-annotate/clientlib/asset-zoom.js
+++ eaem-64-extensions/eaem-touchui-asset-zoom-in-annotate/jcr_root/apps/eaem-touchui-asset-zoom-in-annotate/clientlib/asset-zoom.js
@@ -3,11 +3,14 @@
 
     var _ = window._,
         eaemZoomAdded = false,
-        TITLE_BAR = ".granite-title",
+        ZOOM_CANVAS_ID = "dam-aasetdetail-zoom-canvas",
+        ASSET_MAIN_IMAGE_ID = "asset-mainimage",
         ANNOTATE_PAGE_URL = "/mnt/overlay/dam/gui/content/assets/annotate.html",
         ZOOM_UI = "/apps/eaem-touchui-asset-zoom-in-annotate/ui/asset-zoom.html";
 
 	$document.on("foundation-contentloaded", addAssetZoom);
+
+    overrideAnnotate();
 
     function addAssetZoom(){
         if(!isAnnotatePage() || !!eaemZoomAdded){
@@ -20,14 +23,51 @@
     }
 
     function addZoomUI(html){
-        var $titleBar = $(TITLE_BAR);
+        html = html.substring(html.indexOf("<div"));
 
         var $zoomContainer = $(html).appendTo($("betty-titlebar-primary"));
 
         $zoomContainer.find("coral-actionbar").css("background-color", "#f0f0f0")
-                        .width("165px").css("height" , "2.5rem").css("padding", "0");
+                        .width("200px").css("height" , "2.5rem").css("padding", "0");
 
-        $document.trigger("foundation-contentloaded");
+        $document.on("click", ".dam-asset-zoomIn", reAddImageToCanvas);
+
+        $document.on("click", ".dam-asset-reset", showMainImage);
+    }
+
+    function showMainImage(){
+        $("#" + ASSET_MAIN_IMAGE_ID).css("display", "inline");
+    }
+
+    function reAddImageToCanvas(){
+        var $mainImage = $("#" + ASSET_MAIN_IMAGE_ID),
+            imageHtml = $mainImage[0].outerHTML;
+
+        $mainImage.css("display", "none");
+
+        if(!_.isEmpty($(ZOOM_CANVAS_ID))){
+            return;
+        }
+
+        $document.bind('DOMNodeInserted', function(e) {
+            var element = e.target;
+
+            if(element.id !== ZOOM_CANVAS_ID){
+                return;
+            }
+
+            $(imageHtml).insertAfter($(element)).css("display", "none");
+        });
+    }
+
+    function overrideAnnotate(){
+        var origFn = $.Annotation.prototype._endDraw;
+
+        $.Annotation.prototype._endDraw = function(e) {
+            origFn.call(this, e);
+
+            $("#" + ASSET_MAIN_IMAGE_ID).trigger("annotateEnd", [ e.data.self ]);
+        };
     }
 
     function isAnnotatePage() {
