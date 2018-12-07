aaf4eb69c3bf57b20b41af135355359b2537bc28
sreek <nalabotu@adobe.com>
2018-12-07 10:03:43 -0600

    crxdev env indicator

---
+++ eaem-64-extensions/eaem-crxde-env-indicator/crxde-lite-env-indicator.js
@@ -0,0 +1,25 @@
+(function(){
+    function changeNavColorCRXDELite(switcher){
+        var message = "!!! CAUTION PUBLISH !!!";
+
+        var inner = switcher.firstChild;
+    	
+    	inner.style.background = "#FFFF33";
+    	
+    	var publishText = '<span style="font-size:30px; font-weight: bold; color:red; margin-left: 700px">' + message +'</span>';
+    	
+    	inner.insertAdjacentHTML('beforeend', publishText);
+    }    
+    
+    Ext.onReady(function(){
+        var INTERVAL = setInterval(function(){
+            var switcher = document.getElementsByClassName("crx-switcher");
+
+            if(switcher.length > 0){
+                clearInterval(INTERVAL);
+                changeNavColorCRXDELite(switcher[0]);
+            }
+        }, 250);
+    });
+}())
+
