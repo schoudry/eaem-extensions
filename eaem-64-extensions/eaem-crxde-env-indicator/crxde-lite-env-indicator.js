3f44ab9ade6c3cd15828693c2a56759d5bbc52b0
sreek <nalabotu@adobe.com>
2018-12-13 11:05:41 -0600

    changes

--- eaem-64-extensions/eaem-crxde-env-indicator/crxde-lite-env-indicator.js
+++ eaem-64-extensions/eaem-crxde-env-indicator/crxde-lite-env-indicator.js
@@ -1,21 +1,21 @@
-(function(){
-    function changeNavColorCRXDELite(switcher){
+(function () {
+    function changeNavColorCRXDELite(switcher) {
         var message = "!!! CAUTION PUBLISH !!!";
 
         var inner = switcher.firstChild;
-    	
-    	inner.style.background = "#FFFF33";
-    	
-    	var publishText = '<span style="font-size:30px; font-weight: bold; color:red; margin-left: 700px">' + message +'</span>';
-    	
-    	inner.insertAdjacentHTML('beforeend', publishText);
-    }    
-    
-    Ext.onReady(function(){
-        var INTERVAL = setInterval(function(){
+
+        inner.style.background = "#FFFF33";
+
+        var publishText = '<span style="font-size:30px; font-weight: bold; color:red; margin-left: 700px">' + message + '</span>';
+
+        inner.insertAdjacentHTML('beforeend', publishText);
+    }
+
+    Ext.onReady(function () {
+        var INTERVAL = setInterval(function () {
             var switcher = document.getElementsByClassName("crx-switcher");
 
-            if(switcher.length > 0){
+            if (switcher.length > 0) {
                 clearInterval(INTERVAL);
                 changeNavColorCRXDELite(switcher[0]);
             }
