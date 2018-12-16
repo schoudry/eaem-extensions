f9f3c20cfc94e7fea82a98acd14c2877c5b10d89
sreek <nalabotu@adobe.com>
2018-12-16 15:06:48 -0600

    changes

--- eaem-64-extensions/eaem-touchui-cfm-font-size-plugin/jcr_root/apps/eaem-touchui-cfm-font-size-plugin/clientlib/cfm-rte-font-size-plugin.js
+++ eaem-64-extensions/eaem-touchui-cfm-font-size-plugin/jcr_root/apps/eaem-touchui-cfm-font-size-plugin/clientlib/cfm-rte-font-size-plugin.js
@@ -2,9 +2,62 @@
     var EAEM_PLUGIN_ID = "eaemfont",
         EAEM_TEXT_FONT_FEATURE = "eaemTextFont",
         EAEM_TEXT_FONT_ICON = EAEM_PLUGIN_ID + "#" + EAEM_TEXT_FONT_FEATURE,
-        FONT_SELECTOR_URL = "/apps/eaem-touchui-cfm-font-size-plugin/font-selector.html";
+        CANCEL_CSS = "[data-foundation-wizard-control-action='cancel']",
+        FONT_SELECTOR_URL = "/apps/eaem-touchui-cfm-font-size-plugin/font-selector.html",
+        SENDER = "experience-aem", $eaemFontPicker,
+        url = document.location.pathname;
 
-    extendStyledTextEditor();
+    if( url.indexOf("/editor.html") == 0 ){
+        extendStyledTextEditor();
+
+        registerPlugin();
+    }else if(url.indexOf(FONT_SELECTOR_URL) == 0){
+        handlePicker();
+    }
+
+    function handlePicker(){
+        $document.on("click", CANCEL_CSS, sendCancelMessage);
+    }
+
+    function sendCancelMessage(){
+        var message = {
+            sender: SENDER,
+            action: "cancel"
+        };
+
+        getParent().postMessage(JSON.stringify(message), "*");
+    }
+
+    function getParent() {
+        if (window.opener) {
+            return window.opener;
+        }
+
+        return parent;
+    }
+
+    function closePicker(event){
+        event = event.originalEvent || {};
+
+        if (_.isEmpty(event.data)) {
+            return;
+        }
+
+        var message = JSON.parse(event.data),
+            action;
+
+        if (!message || message.sender !== SENDER) {
+            return;
+        }
+
+        action = message.action;
+
+        if(action === "cancel"){
+            var modal = $eaemFontPicker.data('modal');
+            modal.hide();
+            modal.$element.remove();
+        }
+    }
 
     function extendStyledTextEditor(){
         var origFn = Dam.CFM.StyledTextEditor.prototype._start;
@@ -25,79 +78,82 @@
         config.uiSettings.cui.multieditorFullscreen.toolbar.push(EAEM_TEXT_FONT_ICON);
     }
 
-    var EAEM_CFM_TEXT_FONT_PLUGIN = new Class({
-        toString: "eaemCFMTextFontPlugin",
+    function registerPlugin(){
+        var EAEM_CFM_TEXT_FONT_PLUGIN = new Class({
+            toString: "eaemCFMTextFontPlugin",
 
-        extend: CUI.rte.plugins.Plugin,
+            extend: CUI.rte.plugins.Plugin,
 
-        textFontUI:  null,
+            textFontUI:  null,
 
-        getFeatures: function () {
-            return [ EAEM_TEXT_FONT_FEATURE ];
-        },
+            getFeatures: function () {
+                return [ EAEM_TEXT_FONT_FEATURE ];
+            },
 
-        notifyPluginConfig: function (pluginConfig) {
-            var defaults = {
-                tooltips: {}
-            };
+            notifyPluginConfig: function (pluginConfig) {
+                var defaults = {
+                    tooltips: {}
+                };
 
-            defaults.tooltips[EAEM_TEXT_FONT_FEATURE] = {
-                title: "Set text font size, color..."
-            };
+                defaults.tooltips[EAEM_TEXT_FONT_FEATURE] = {
+                    title: "Set text font size, color..."
+                };
 
-            CUI.rte.Utils.applyDefaults(pluginConfig, defaults);
+                CUI.rte.Utils.applyDefaults(pluginConfig, defaults);
 
-            this.config = pluginConfig;
-        },
+                this.config = pluginConfig;
+            },
 
-        initializeUI: function (tbGenerator) {
-            if (!this.isFeatureEnabled(EAEM_TEXT_FONT_FEATURE)) {
-                return;
+            initializeUI: function (tbGenerator) {
+                if (!this.isFeatureEnabled(EAEM_TEXT_FONT_FEATURE)) {
+                    return;
+                }
+
+                this.textFontUI = new tbGenerator.createElement(EAEM_TEXT_FONT_FEATURE, this,
+                    false, this.config.tooltips[EAEM_TEXT_FONT_FEATURE]);
+
+                tbGenerator.addElement(EAEM_TEXT_FONT_FEATURE, 999, this.textFontUI, 999);
+
+                if (tbGenerator.registerIcon) {
+                    tbGenerator.registerIcon(EAEM_TEXT_FONT_ICON, "textColor");
+                }
+
+                $(window).off('message', closePicker)
+                    .on('message', closePicker);
+            },
+
+            execute: function (pluginCommand, value, envOptions) {
+                var context = envOptions.editContext;
+
+                if (pluginCommand != EAEM_TEXT_FONT_FEATURE) {
+                    return;
+                }
+
+                this.insertTextAttributes(context);
+            },
+
+            insertTextAttributes: function(context){
+                this.showFontModal();
+            },
+
+            showFontModal: function(){
+                var $iframe = $('<iframe>'),
+                    $modal = $('<div>').addClass('eaem-cfm-font-size coral-Modal');
+
+                $iframe.attr('src', Granite.HTTP.externalize(FONT_SELECTOR_URL)).appendTo($modal);
+
+                $modal.appendTo('body').modal({
+                    type: 'default',
+                    buttons: [],
+                    visible: true
+                });
+
+                $eaemFontPicker = $modal;
+
+                $modal.nextAll(".coral-Modal-backdrop").addClass("cfm-coral2-backdrop");
             }
+        });
 
-            this.textFontUI = new tbGenerator.createElement(EAEM_TEXT_FONT_FEATURE, this,
-                                                    false, this.config.tooltips[EAEM_TEXT_FONT_FEATURE]);
-
-            tbGenerator.addElement(EAEM_TEXT_FONT_FEATURE, 999, this.textFontUI, 999);
-
-            if (tbGenerator.registerIcon) {
-                tbGenerator.registerIcon(EAEM_TEXT_FONT_ICON, "textColor");
-            }
-        },
-
-        execute: function (pluginCommand, value, envOptions) {
-            var context = envOptions.editContext;
-
-            if (pluginCommand != EAEM_TEXT_FONT_FEATURE) {
-                return;
-            }
-
-            this.insertTextAttributes(context);
-        },
-
-        insertTextAttributes: function(context){
-            this.showFontModal();
-        },
-
-        showFontModal: function(){
-            var $iframe = $('<iframe>'),
-                $modal = $('<div>').addClass('eaem-cfm-font-size coral-Modal');
-
-            $iframe.attr('src', Granite.HTTP.externalize(FONT_SELECTOR_URL)).appendTo($modal);
-
-            $modal.appendTo('body').modal({
-                type: 'default',
-                buttons: [],
-                visible: true
-            });
-
-            $modal._callback = function(){
-                alert("hi")
-            };
-
-            $modal.nextAll(".coral-Modal-backdrop").addClass("cfm-coral2-backdrop");
-        }
-    });
-
-    CUI.rte.plugins.PluginRegistry.register(EAEM_PLUGIN_ID, EAEM_CFM_TEXT_FONT_PLUGIN);
+        CUI.rte.plugins.PluginRegistry.register(EAEM_PLUGIN_ID, EAEM_CFM_TEXT_FONT_PLUGIN);
+    }
 }(jQuery, jQuery(document)));
