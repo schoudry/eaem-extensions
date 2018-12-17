15bbf32e6964d1f082016aefb58222b183f78bc4
sreek <nalabotu@adobe.com>
2018-12-17 08:04:23 -0600

    changes

--- eaem-64-extensions/eaem-touchui-cfm-font-size-plugin/jcr_root/apps/eaem-touchui-cfm-font-size-plugin/clientlib/cfm-rte-font-size-plugin.js
+++ eaem-64-extensions/eaem-touchui-cfm-font-size-plugin/jcr_root/apps/eaem-touchui-cfm-font-size-plugin/clientlib/cfm-rte-font-size-plugin.js
@@ -17,6 +17,23 @@
 
     function handlePicker(){
         $document.on("click", CANCEL_CSS, sendCancelMessage);
+
+        $document.submit(sentTextAttributes);
+    }
+
+    function sentTextAttributes(){
+        var message = {
+            sender: SENDER,
+            action: "submit",
+            data: {}
+        }, $form = $("form"), $field;
+
+        _.each($form.find("[name^='./']"), function(field){
+            $field = $(field);
+            message.data[$field.attr("name").substr(2)] = $field.val();
+        });
+
+        parent.postMessage(JSON.stringify(message), "*");
     }
 
     function sendCancelMessage(){
@@ -43,8 +60,13 @@
             return;
         }
 
-        var message = JSON.parse(event.data),
-            action;
+        var message, action;
+
+        try{
+            message = JSON.parse(event.data);
+        }catch(err){
+            return;
+        }
 
         if (!message || message.sender !== SENDER) {
             return;
@@ -52,11 +74,13 @@
 
         action = message.action;
 
-        if(action === "cancel"){
-            var modal = $eaemFontPicker.data('modal');
-            modal.hide();
-            modal.$element.remove();
+        if(action === "submit"){
+            $eaemFontPicker.eaemFontPlugin.editorKernel.execCmd(EAEM_TEXT_FONT_FEATURE, message.data);
         }
+
+        var modal = $eaemFontPicker.data('modal');
+        modal.hide();
+        modal.$element.remove();
     }
 
     function extendStyledTextEditor(){
@@ -118,8 +142,12 @@
                     tbGenerator.registerIcon(EAEM_TEXT_FONT_ICON, "textColor");
                 }
 
-                $(window).off('message', closePicker)
-                    .on('message', closePicker);
+                $(window).off('message', closePicker).on('message', closePicker);
+            },
+
+            isValidSelection: function(){
+                var winSel = window.getSelection();
+                return winSel && winSel.rangeCount == 1 && winSel.getRangeAt(0).toString().length > 0;
             },
 
             execute: function (pluginCommand, value, envOptions) {
@@ -129,15 +157,15 @@
                     return;
                 }
 
-                this.insertTextAttributes(context);
-            },
+                if(!this.isValidSelection()){
+                    return;
+                }
 
-            insertTextAttributes: function(context){
                 this.showFontModal();
             },
 
             showFontModal: function(){
-                var $iframe = $('<iframe>'),
+                var self = this, $iframe = $('<iframe>'),
                     $modal = $('<div>').addClass('eaem-cfm-font-size coral-Modal');
 
                 $iframe.attr('src', Granite.HTTP.externalize(FONT_SELECTOR_URL)).appendTo($modal);
@@ -150,10 +178,79 @@
 
                 $eaemFontPicker = $modal;
 
+                $eaemFontPicker.eaemFontPlugin = self;
+
                 $modal.nextAll(".coral-Modal-backdrop").addClass("cfm-coral2-backdrop");
             }
         });
 
+        var EAEM_CFM_TEXT_FONT_CMD = new Class({
+            toString: "eaemCFMTextFontCmd",
+
+            extend: CUI.rte.commands.Command,
+
+            isCommand: function (cmdStr) {
+                return (cmdStr.toLowerCase() == EAEM_TEXT_FONT_FEATURE);
+            },
+
+            getProcessingOptions: function () {
+                var cmd = CUI.rte.commands.Command;
+                return cmd.PO_SELECTION | cmd.PO_BOOKMARK | cmd.PO_NODELIST;
+            },
+
+            getTagObject: function(size, color) {
+                var style = "";
+
+                if(!_.isEmpty(color)){
+                    style = "color: " + color + ";";
+                }
+
+                if(!_.isEmpty(size)){
+                    style = style + "font-size: " + size;
+                }
+
+                return {
+                    "tag": "span",
+                    "attributes": {
+                        "style" : style
+                    }
+                };
+            },
+
+            execute: function (execDef) {
+                var textData = execDef.value, selection = execDef.selection,
+                    nodeList = execDef.nodeList;
+
+                if (!selection || !nodeList) {
+                    return;
+                }
+
+                var common = CUI.rte.Common,
+                    context = execDef.editContext,
+                    tagObj = this.getTagObject(textData.size, textData.color);
+
+                if(_.isEmpty(textData)){
+                    nodeList.removeNodesByTag(execDef.editContext, tagObj.tag, undefined, true);
+                    return;
+                }
+
+                var tags = common.getTagInPath(context, selection.startNode, tagObj.tag);
+
+                //remove existing color before adding new color
+                if (tags != null) {
+                    nodeList.removeNodesByTag(execDef.editContext, tagObj.tag, undefined, true);
+                }
+
+                nodeList.surround(execDef.editContext, tagObj.tag, tagObj.attributes);
+            },
+
+            queryState: function(selectionDef, cmd) {
+                return false;
+            }
+        });
+
         CUI.rte.plugins.PluginRegistry.register(EAEM_PLUGIN_ID, EAEM_CFM_TEXT_FONT_PLUGIN);
+
+        CUI.rte.commands.CommandRegistry.register(EAEM_TEXT_FONT_FEATURE, EAEM_CFM_TEXT_FONT_CMD);
     }
 }(jQuery, jQuery(document)));
