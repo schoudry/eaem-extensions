9749fdc35bac3a90cc237a3054a0cb873955ca5c
sreek <nalabotu@adobe.com>
2019-03-14 11:40:44 -0500

    changes

---
+++ eaem-64-extensions/eaem-most-popular-content-fragments/content/src/main/content/jcr_root/apps/eaem-most-popular-content-fragments/clientlib/show-fragment-views.js
@@ -0,0 +1,14 @@
+(function ($, $document) {
+    "use strict";
+ 
+    var firstLoad = true,
+        COOKIE_AEM_ASSETS_LIST_VIEW_COLUMNS = "aem.assets.listview.columns",
+        EAEM_ASSETS_COUNT = "eaemAssetsCount",
+        LAYOUT_COL_VIEW = "column",
+        LAYOUT_LIST_VIEW = "list",
+        LAYOUT_CARD_VIEW = "card",
+        DIRECTORY = "directory",
+        FOUNDATION_CONTENT_LOADED = "foundation-contentloaded",
+        SEL_DAM_ADMIN_CHILD_PAGES = ".cq-damadmin-admin-childpages";
+
+})(jQuery, jQuery(document));
