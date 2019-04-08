ad0e3fa3bf069c705f55424ba6c6e201e6d99465
sreek <nalabotu@adobe.com>
2019-04-08 14:05:53 -0500

    image mf

--- eaem-64-extensions/eaem-touchui-image-multifield/jcr_root/apps/eaem-touchui-image-multifield/clientlib/image-multifield.js
+++ eaem-64-extensions/eaem-touchui-image-multifield/jcr_root/apps/eaem-touchui-image-multifield/clientlib/image-multifield.js
@@ -1,5 +1,8 @@
 (function ($, $document) {
-    var registry = $(window).adaptTo("foundation-registry"),
+    var COMPOSITE_MULTIFIELD_SELECTOR = "coral-multifield[data-granite-coral-multifield-composite]",
+        FILE_REFERENCE_PARAM = "fileReference",
+        registry = $(window).adaptTo("foundation-registry"),
+        ALLOWED_MIME_TYPE = "image/jpeg",
         adapters = registry.get("foundation.adapters");
 
     var fuAdapter = _.reject(adapters, function(adapter){
@@ -32,4 +35,74 @@
             }
         });
     };
+
+    $document.on("foundation-contentloaded", function(e) {
+        var composites = $(COMPOSITE_MULTIFIELD_SELECTOR, e.target);
+
+        composites.each(function() {
+            Coral.commons.ready(this, function(el) {
+                addThumbnails(el);
+            });
+        });
+    });
+
+    function addThumbnails(multifield){
+        var $multifield = $(multifield),
+            dataPath = $multifield.closest(".cq-dialog").attr("action"),
+            mfName = $multifield.attr("data-granite-coral-multifield-name");
+
+        dataPath = dataPath + "/" + getStringAfterLastSlash(mfName);
+
+        $.ajax({
+            url: dataPath + ".2.json",
+            cache: false
+        }).done(handler);
+
+        function handler(mfData){
+            multifield.items.getAll().forEach(function(item, i) {
+                var $mfItem = $(item),
+                    $fileUpload = $mfItem.find("coral-fileupload");
+
+                if(_.isEmpty($fileUpload)){
+                    return;
+                }
+
+                var itemName = getJustItemName($fileUpload.attr("name"));
+
+                if(_.isEmpty(mfData[itemName]) || _.isEmpty((mfData[itemName][FILE_REFERENCE_PARAM]))){
+                    return;
+                }
+
+                var imagePath = mfData[itemName][FILE_REFERENCE_PARAM];
+
+                $fileUpload.trigger($.Event("assetselected", {
+                    path: imagePath,
+                    group: "",
+                    mimetype: ALLOWED_MIME_TYPE, // workaround to add thumbnail
+                    param: "",
+                    thumbnail: getThumbnailHtml(imagePath)
+                }));
+            });
+        }
+
+        function getThumbnailHtml(path){
+            return "<img class='cq-dd-image' src='" + path + "/_jcr_content/renditions/cq5dam.thumbnail.319.319.png'>";
+        }
+
+        function getJustItemName(itemName){
+            itemName = itemName.substr(itemName.indexOf(mfName) + mfName.length + 1);
+
+            itemName = itemName.substring(0, itemName.indexOf("/"));
+
+            return itemName;
+        }
+    }
+
+    function getStringAfterLastSlash(str){
+        if(!str || (str.indexOf("/") == -1)){
+            return "";
+        }
+
+        return str.substr(str.lastIndexOf("/") + 1);
+    }
 }(jQuery, jQuery(document)));
