16bdf1b01658fbcc1c76909abaca27bec09ddbdf
sreek <nalabotu@adobe.com>
2019-04-09 16:36:43 -0500

    cfm ml required

--- eaem-64-extensions/eaem-cfm-multi-line-text-required-validator/jcr_root/apps/eaem-cfm-multi-line-text-required-validator/clientlib/multiline-required.js
+++ eaem-64-extensions/eaem-cfm-multi-line-text-required-validator/jcr_root/apps/eaem-cfm-multi-line-text-required-validator/clientlib/multiline-required.js
@@ -1,12 +1,25 @@
 (function ($, $document) {
     var CFM = window.Dam.CFM,
         EAEM_INVISIBLE_CLASS = "eaem-text-invisible",
-        SELECTOR = "textarea.plaintext-editor, textarea.markdown-editor, ." + EAEM_INVISIBLE_CLASS,
+        PLAIN_MARKDOWN_SELECTOR = "textarea.plaintext-editor, textarea.markdown-editor",
         registry = $(window).adaptTo("foundation-registry");
 
+    //for plain text, markdown
     registry.register("foundation.validation.validator", {
-        selector: SELECTOR,
+        selector: PLAIN_MARKDOWN_SELECTOR,
         validate: validator
+    });
+
+    //for richtext
+    registry.register("foundation.validation.validator", {
+        selector:  "." + EAEM_INVISIBLE_CLASS,
+        validate: validator,
+        show: function (invisibleText, message) {
+            $(invisibleText).prevAll("[data-cfm-richtext-editable]").css("border-color", "#e14132");
+        },
+        clear: function (invisibleText) {
+            $(invisibleText).prevAll("[data-cfm-richtext-editable]").css("border-color", "#d0d0d0");
+        }
     });
 
     CFM.Core.registerReadyHandler(registerRTEValidator);
@@ -27,15 +40,21 @@
         if(!_.isEmpty($rte)){
             //coral validation framework ignores hidden and contenteditable fields, so add an invisible text field
             //the text field is just for registering a validator
-            var $eaemCopyField = $("<input type=text style='display:inline' class='" + EAEM_INVISIBLE_CLASS + "'/>")
-                .insertAfter($rte);
+            var $eaemCopyField = $("<input type=text style='display:none' class='" + EAEM_INVISIBLE_CLASS + "'/>")
+                                        .insertAfter($rte);
+
+            var $rteEditable = $rte.prev("[data-cfm-richtext-editable]");
+
+            $eaemCopyField.val($rteEditable.text().trim());
+
+            checkValidity.call($eaemCopyField[0]);
 
             $rte.prev("[data-cfm-richtext-editable]").on("input", function() {
                 $eaemCopyField.val($(this).text().trim());
                 checkValidity.call($eaemCopyField[0]);
             });
-
-            checkValidity.call($eaemCopyField[0]);
+        }else{
+            //$(PLAIN_MARKDOWN_SELECTOR).each(checkValidity);
         }
     }
 
