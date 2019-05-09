92d4d9695e4530d234abd21ee02382d98967dec1
sreek <nalabotu@adobe.com>
2019-05-09 16:35:04 -0500

    changes

--- eaem-64-extensions/eaem-dms7-audio-video-component/jcr_root/apps/eaem-dms7-audio-video-component/eaem-dms7-audio-video-component/audio-video.js
+++ eaem-64-extensions/eaem-dms7-audio-video-component/jcr_root/apps/eaem-dms7-audio-video-component/eaem-dms7-audio-video-component/audio-video.js
@@ -1,31 +1,49 @@
 use( function(){
-    var fileReference = properties['./fileReference'],
+    var METADATA_NODE = com.day.cq.dam.api.s7dam.constants.S7damConstants.S7_ASSET_METADATA_NODE,
+        SCENE7_FODLER = METADATA_NODE + "/metadata/dam:scene7Folder",
+        SCENE7_DOMAIN = METADATA_NODE + "/metadata/dam:scene7Domain",
+        fileReference = properties['./fileReference'],
         rootNode = currentSession.rootNode,
-        assetNode;
+        mediaInfo = {};
 
-    log.info("fileReference----------" + fileReference);
-
-    if(fileReference != undefined){
-        assetNode = rootNode.getNode(fileReference.substring(1));
+    if(fileReference == undefined) {
+        mediaInfo.isAudio = false;
+        return;
     }
+
+    var assetNode = rootNode.getNode(fileReference.substring(1)),
+        publishUrl = getPublishServerURL(assetNode);
+
+    //log.info("publishUrl----------" + publishUrl);
 
     return{
-        isAudio: isAudioFile(assetNode)
-    }
-});
+        isAudio: isAudioFile(assetNode),
+        assetName: assetNode.getName(),
+        s7PublishPath: publishUrl,
+        bgImage: assetNode.getPath() + "/jcr:content/renditions/poster.png"
+    };
 
-function isAudioFile(assetNode){
-    var META_DC_FORMAT = "jcr:content/metadata/dc:format",
-        isAudioFile = false;
+    function isAudioFile(assetNode){
+        var META_DC_FORMAT = "jcr:content/metadata/dc:format",
+            isAudioFile = false;
 
-    if(assetNode == undefined){
+        if(assetNode == undefined){
+            return isAudioFile;
+        }
+
+        if( assetNode.hasProperty(META_DC_FORMAT)) {
+            var dcFormat = assetNode.getProperty(META_DC_FORMAT).getString();
+            isAudioFile = dcFormat.startsWith("audio");
+        }
+
         return isAudioFile;
     }
 
-    if( assetNode.hasProperty(META_DC_FORMAT)) {
-        var dcFormat = assetNode.getProperty(META_DC_FORMAT).getString();
-        isAudioFile = dcFormat.startsWith("audio");
-    }
+    function getPublishServerURL(assetNode) {
+        var s7Path = assetNode.getProperty(SCENE7_DOMAIN).getString() + "is/content/"
+                        + assetNode.getProperty(SCENE7_FODLER).getString() + assetNode.getName();
 
-    return isAudioFile;
-}
+        return s7Path;
+    }
+});
+
