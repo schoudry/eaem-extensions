65dda7c1c99bd17e3906bc8b05237598a4129be8
sreek <nalabotu@adobe.com>
2019-06-06 14:23:25 -0500

    changes

--- eaem-65-extensions/eaem-site-pages-html-to-pdf/bundle/src/main/java/apps/experienceaem/sites/EAEMCreatePDFFromPage.java
+++ eaem-65-extensions/eaem-site-pages-html-to-pdf/bundle/src/main/java/apps/experienceaem/sites/EAEMCreatePDFFromPage.java
@@ -3,6 +3,7 @@ package apps.experienceaem.sites;
 import com.adobe.granite.workflow.PayloadMap;
 import com.day.cq.commons.jcr.JcrUtil;
 import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.AssetManager;
 import com.day.cq.dam.commons.util.DamUtil;
 import com.day.cq.replication.Agent;
 import com.day.cq.replication.AgentManager;
@@ -12,6 +13,7 @@ import com.day.cq.workflow.exec.WorkflowData;
 import com.day.cq.workflow.metadata.MetaDataMap;
 import org.apache.commons.exec.*;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.*;
 import com.day.cq.workflow.WorkflowException;
@@ -27,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import javax.jcr.Node;
 import javax.jcr.Session;
 import java.io.File;
+import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -44,6 +47,9 @@ public class EAEMCreatePDFFromPage implements WorkflowProcess {
     private static Integer CMD_TIME_OUT = 300000; // 5 minutes
     private static SimpleDateFormat PDF_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
     private static String AGENT_PUBLISH = "publish";
+    private static String DAM_FOLDER_PREFIX = "/content/dam";
+    private static int RETRIES = 20;
+    private static int SLEEP_TIME = 5000; // wait 5 secs
 
     @Reference
     private ResourceResolverFactory resolverFactory;
@@ -77,19 +83,32 @@ public class EAEMCreatePDFFromPage implements WorkflowProcess {
             }
 
             ResourceResolver resolver = getResourceResolver(session);
+            Resource pageResource = resolver.getResource(pagePath);
 
             tmpDir = File.createTempFile("eaem", null);
             tmpDir.delete();
             tmpDir.mkdir();
 
-            File tmpFile = new File(tmpDir, resolver.getResource(pagePath).getName()
-                                            + "-" + PDF_DATE_FORMAT.format(new Date()) + ".pdf");
-
+            File tmpFile = new File(tmpDir, pageResource.getName() + "-" + PDF_DATE_FORMAT.format(new Date()) + ".pdf");
             CommandLine commandLine = getCommandLine(pagePath, args, tmpFile);
+            int count = RETRIES;
 
-            executeCommand(commandLine);
+            do{
+                Thread.sleep(SLEEP_TIME);
 
-            session.save();
+                if( !wasPageReplicatedRecently(pageResource) ){
+                    log.debug("Page - " + pageResource.getPath() + ", not replicated, skipping PDF generation");
+                    continue;
+                }
+
+                executeCommand(commandLine);
+
+                createAssetInDAM(pagePath, tmpFile, resolver);
+
+                session.save();
+
+                break;
+            }while(count-- > 0);
         } catch (Exception e) {
             log.error("Failed to create PDF of page", e);
         }finally{
@@ -97,6 +116,42 @@ public class EAEMCreatePDFFromPage implements WorkflowProcess {
                 try { FileUtils.deleteDirectory(tmpDir); } catch(Exception ignore){}
             }
         }
+    }
+
+    private boolean wasPageReplicatedRecently(Resource pageResource){
+        ValueMap valueMap = pageResource.getChild("jcr:content").getValueMap();
+        Date lastReplicated = valueMap.get("cq:lastReplicated", Date.class);
+
+        if(lastReplicated == null){
+            return false;
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.HOUR, -1);
+
+        Date oneHourBack = cal.getTime();
+
+        //check if the page was replicated in last 60 minutes
+        return lastReplicated.getTime() > oneHourBack.getTime();
+    }
+
+    private void createAssetInDAM(String pagePath, File tmpFile, ResourceResolver resolver)throws Exception{
+        String rootPath = DAM_FOLDER_PREFIX + pagePath;
+        String assetPath = rootPath + "/" + tmpFile.getName();
+
+        JcrUtil.createPath(DAM_FOLDER_PREFIX + pagePath, "sling:Folder", "sling:Folder",
+                                    resolver.adaptTo(Session.class), true);
+
+        AssetManager assetManager = resolver.adaptTo(AssetManager.class);
+
+        FileInputStream fileIn = new FileInputStream(tmpFile);
+
+        Asset asset = assetManager.createAsset(assetPath, fileIn, "application/pdf", false);
+
+        log.info("Created asset - " + asset.getPath() + ", for published page - " + pagePath);
+
+        IOUtils.closeQuietly(fileIn);
     }
 
     private CommandLine getCommandLine(String pagePath, MetaDataMap args, File tmpFile) throws Exception{
