1fdd9ace9c3935c130e896ea01d9f341d1fb7b16
sreek <nalabotu@adobe.com>
2019-06-13 10:39:52 -0500

    tags picker

---
+++ eaem-65-extensions/eaem-touchui-tags-picker-show-full-title/jcr_root/apps/eaem-touchui-tags-picker-show-full-title/clientlib/show-full-title.js
@@ -0,0 +1,61 @@
+(function($, $document) {
+    var TAGS_FIELD = "cq:tags",
+        LETTER_COUNT = 22, INCREASE_BY = 1.5, CV_ITEM_HEIGHT = 3, CV_LABEL_HEIGHT = 2,
+        extended = false;
+
+    $document.on("foundation-contentloaded", handleTagsPicker);
+
+    function handleTagsPicker(){
+        if(extended){
+            return;
+        }
+
+        var $tagsField = $("foundation-autocomplete[name$='" + TAGS_FIELD + "']");
+
+        if(_.isEmpty($tagsField)){
+            return;
+        }
+
+        var pathField = $tagsField[0];
+
+        extended = true;
+
+        extendPicker(pathField);
+    }
+
+    function extendPicker(pathField){
+        var origShowPicker = pathField._showPicker;
+
+        pathField._showPicker = function(){
+            origShowPicker.call(this);
+
+            var columnView = $(this._picker.el).find("coral-columnview")[0];
+
+            columnView.on("coral-columnview:navigate", showFullTitle);
+
+            var dummyEvent = { detail : { column: $(columnView).find("coral-columnview-column")[0] } };
+
+            showFullTitle(dummyEvent);
+        }
+    }
+
+    function showFullTitle(event){
+        var $item, $content, increase, $cvItem;
+
+        $(event.detail.column).find("coral-columnview-item").each(function(index, item){
+            $item = $(item);
+
+            $content = $item.find("coral-columnview-item-content");
+
+            increase = (INCREASE_BY * Math.floor($content.html().length / LETTER_COUNT));
+
+            if( ($item.prop("variant") == "drilldown") && (increase > 0)){
+                increase++;
+            }
+
+            $item.css("height", (CV_ITEM_HEIGHT + increase) + "rem");
+
+            $content.css("height",(CV_LABEL_HEIGHT + increase) + "rem").css("white-space", "normal");
+        });
+    }
+})(jQuery, jQuery(document));
