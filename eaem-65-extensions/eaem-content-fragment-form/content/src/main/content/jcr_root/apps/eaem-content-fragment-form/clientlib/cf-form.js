4bcee6e9eea4f76cbf1b08b07ce92fb37e8ee2e6
nalabotu <nalabotu@adobe.com>
2020-01-09 10:32:59 -0600

    changes

--- eaem-65-extensions/eaem-content-fragment-form/content/src/main/content/jcr_root/apps/eaem-content-fragment-form/clientlib/cf-form.js
+++ eaem-65-extensions/eaem-content-fragment-form/content/src/main/content/jcr_root/apps/eaem-content-fragment-form/clientlib/cf-form.js
@@ -1,6 +1,6 @@
 (function($, $document) {
     var PLAYABLE_MEDIA_PAGE = "/apps/eaem-content-fragment-form/playable-media.html",
-        formTabAdded = false;
+        formTabAdded = false, initialData;
 
     $document.on("foundation-contentloaded", addFragmentFormTab);
 
@@ -51,6 +51,46 @@
         });
 
         addFormActions();
+
+        saveInitialState();
+
+        addTabNavigationAlert();
+    }
+
+    function addTabNavigationAlert(){
+        var tabClickHandler = getTabClickHandler();
+
+        $document.off('click', '#SidePanel coral-tab');
+
+        $document.on('click', '#SidePanel coral-tab', function( eve ) {
+            var that = this;
+
+            if (initialData === $("form").serialize()) {
+                tabClickHandler.call(that);
+                return;
+            }
+
+            var fui = $(window).adaptTo("foundation-ui");
+
+            fui.prompt("Confirm", "NEW Warning! You must save your work before navigating to a new screen. Click ok to go to the page without saving", "warning", [{
+                text: "Ok",
+                handler: function(){
+                    tabClickHandler.call(that)
+                }
+            }, {
+                text: "Stay",
+                primary: true,
+                handler: function(){}
+            }]);
+        });
+    }
+
+    function getTabClickHandler(){
+        var handlers = $._data(document, "events")["click"];
+
+        return _.reject(handlers, function(handler){
+            return (handler.selector != "#SidePanel coral-tab" );
+        })[0].handler;
     }
 
     function getFragmentFormTabContent(){
@@ -73,5 +113,9 @@
                 document.location.href = href;
             }
         });
+    }
+
+    function saveInitialState(){
+        initialData = $("form").serialize();
     }
 })(jQuery, jQuery(document));
