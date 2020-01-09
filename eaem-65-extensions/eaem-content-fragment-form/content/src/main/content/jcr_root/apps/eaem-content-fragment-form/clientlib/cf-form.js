c23f247eb59019f80830f9e3632baaefcb845638
nalabotu <nalabotu@adobe.com>
2020-01-09 12:11:13 -0600

    changes

--- eaem-65-extensions/eaem-content-fragment-form/content/src/main/content/jcr_root/apps/eaem-content-fragment-form/clientlib/cf-form.js
+++ eaem-65-extensions/eaem-content-fragment-form/content/src/main/content/jcr_root/apps/eaem-content-fragment-form/clientlib/cf-form.js
@@ -6,9 +6,30 @@
         VIDEO_GSP_BROWSER_BUTTON = "video-gsp-browser-button",
         AUDIO_GSP_BROWSER_BUTTON = "audio-gsp-browser-button",
         TRANSCRIPT_GSP_BROWSER_BUTTON = "transcripts-gsp-browser-button",
+        MGID_FILE_URI = "mgidFileURI",
         formTabAdded = false, initialData;
 
-    $document.on("foundation-contentloaded", addFragmentFormTab);
+    var PROFILE_MAPPING = {
+        baseline: '42E0',
+        main: '4D40',
+        high: '6400',
+        extended: '58A0'
+    };
+
+    var FORMAT_MAPPING = {
+        'h264': 'avc1'
+    };
+
+    function showMessage(title, message, callback){
+        var fui = $(window).adaptTo("foundation-ui"),
+            options = [{
+                id: "ok",
+                text: "OK",
+                primary: true
+            }];
+
+        fui.prompt(title, message, "default", options, callback);
+    }
 
     function addFormActions(){
         $(".button-apply").on("click", function(e) {
@@ -146,13 +167,218 @@
         });
 
         $(browse).addClass(buttonCss).css("margin-left", "10px").click(function(){
-            alert("hi");
+            showGSPDialog($accrItem);
         });
 
         $mfAdd.after(browse);
     }
 
+    function getPreviewLinkHtml(link){
+        return  '<a class="coral3-Button coral3-Button--secondary" target="_blank" href=' + link + '>' +
+                    '<coral-button-label>Preview</coral-button-label>' +
+                '</a>';
+    }
+
+    function getPreviewLink(mgid) {
+        var url = "https://mediabus.mtvnservices.com/mediabus-webservices/mediaInspection/getMediaMetadata",
+            xml = "<getMediaAttributes><url>" + mgid + "</url></getMediaAttributes>",
+            httpUrl = "";
+
+        $.ajax({
+            async: false,
+            type: "POST",
+            url: url,
+            data: xml,
+            contentType: 'application/xml',
+            dataType: 'xml',
+            statusCode: {
+                200: function(xmlDoc){
+                    if (xmlDoc.getElementsByTagName("httpurl").length > 0) {
+                        httpUrl = xmlDoc.getElementsByTagName("httpurl").item(0).innerHTML;
+                    }
+                }
+            }
+        });
+
+        return httpUrl;
+    }
+
+    function addPreviewLink($mgIdField){
+        var $previewLink = $('<div/>').appendTo($mgIdField),
+            value = $mgIdField.find("input").val();
+
+        if(_.isEmpty(value)){
+            return;
+        }
+
+        var link = getPreviewLink(value);
+
+        if(_.isEmpty(link)){
+            return;
+        }
+
+        $previewLink.append(getPreviewLinkHtml(link));
+    }
+
+    function fillMultifieldItems(mfItem, mfMap){
+        if( (mfItem === null) || _.isEmpty(mfMap)){
+            return;
+        }
+
+        var fieldNameSuffix = null;
+
+        _.each(mfMap, function(fValue, fKey){
+            fieldNameSuffix = fKey;
+
+            if(fKey === "mgid"){
+                fieldNameSuffix = MGID_FILE_URI;
+            }else if(fKey === "bitRate"){
+                fieldNameSuffix = "avgBitRateKbps";
+            }else if(fKey === "format"){
+                fieldNameSuffix = "videoCodec";
+            } else if (fKey === "container") {
+                if (fValue === "xml") {
+                    fValue = "ttml";
+                }
+            }
+
+            var field = mfItem.querySelector("[name$='" + fieldNameSuffix + "']");
+
+            if(field === null){
+                return;
+            }
+
+            field.value = fValue;
+        });
+
+        var $mgIdField = $(mfItem.querySelector("[name$='" + MGID_FILE_URI + "']"));
+
+        addPreviewLink($mgIdField.closest(".coral-Form-fieldwrapper"));
+    }
+
+    function setVideoCodecFormat(selectedFiles) {
+        if(_.isEmpty(selectedFiles)){
+            return;
+        }
+
+        selectedFiles.forEach(function(fileData) {
+            if (fileData && fileData.format && fileData.profile) {
+                fileData.videoCodec = FORMAT_MAPPING[fileData.format] + '.' + PROFILE_MAPPING[fileData.profile];
+            }
+        });
+    }
+
+    function showGSPDialog($mField) {
+        var ACCEPT_BUTTON = "assetRefsGSPAcceptButton";
+
+        var dialog = new Coral.Dialog().set({
+            header: {
+                innerHTML: 'GSP Browser'
+            },
+            content: {
+                innerHTML: '<div ng-app="gspAppForAssetRefs" class="browser-container" ng-controller="GspCtrl">'+
+                '<arc-gsp-browser options="gspOptions"></arc-gsp-browser>'+
+                '</div>'
+            },
+            footer: {
+                innerHTML: '<button is="coral-button" variant="default" coral-close>Cancel</button>' +
+                '<button id="' + ACCEPT_BUTTON + '" is="coral-button" variant="default" disabled>Select</button>'
+            }
+        });
+
+        document.body.appendChild(dialog);
+
+        dialog.show();
+
+        var app = angular.module("gspAppForAssetRefs", ['gspBrowser']),
+            selectButton = dialog.footer.querySelector('#' + ACCEPT_BUTTON),
+            selectedFileJSON = null;
+
+        // optional
+        app.constant('GSPConfig', {
+            siteKey: 'comedy', // initial used sitekey
+            addContentAttributes: true,
+            allowSitekeySwitch: true, // optional
+            fileTypes: {
+                transcript: ['xml', 'scc'],
+                video: ['flv','mp4', 'fmp4']
+            }
+        });
+
+        dialog.on('coral-overlay:close', function(){
+            try{
+                var selectedFiles, item, content, assetMField;
+
+                if(_.isEmpty(selectedFileJSON)){
+                    return;
+                }
+
+                selectedFiles = JSON.parse(selectedFileJSON);
+
+                assetMField = $mField[0];
+
+                dumpItemsInMultifield($mField);
+
+                _.each(selectedFiles, function(dataItem, index){
+                    $('#viacom-import-count').html(index + 1);
+
+                    item = new Coral.Multifield.Item();
+
+                    content = document.importNode(assetMField.template.content, true).firstElementChild;
+
+                    item.content.appendChild(content);
+
+                    assetMField.items.add(item);
+
+                    fillMultifieldItems(item, dataItem);
+                });
+            }catch(err){
+                showAlert("Error", "Unable to add references for selected : " + selectedFileJSON);
+            }
+        });
+
+        dialog.on('click', '#' + ACCEPT_BUTTON, function() {
+            var selectedFiles = JSON.parse(selectedFileJSON),
+                message = "Adding <span id='viacom-import-count'>1</span> of total " + selectedFiles.length;
+
+            showMessage("Importing...", message);
+
+            dialog.hide();
+        });
+
+        app.controller("GspCtrl", ['$scope', function($scope) {
+            $scope.gspOptions = {
+                shownFileTypes: ['video', 'transcript'],
+                multiSelect: true,
+                onFileSelect: function(selectedFiles) {
+                    if (!_.isEmpty(selectedFiles)) {
+                        setVideoCodecFormat(selectedFiles);
+                        selectedFileJSON = JSON.stringify(selectedFiles);
+                        selectButton.removeAttribute("disabled");
+                    } else {
+                        selectedFileJSON = undefined;
+                        selectButton.setAttribute("disabled", true);
+                    }
+                }
+            };
+        }]);
+
+        angular.bootstrap(dialog, ['gspAppForAssetRefs']);
+    }
+
+    function dumpItemsInMultifield($mField){
+        if(_.isEmpty($mField)){
+            return;
+        }
+
+        _.each($mField[0].items.getAll(), function(mfItem){
+            mfItem.remove();
+        });
+    }
+
     function saveInitialState(){
         initialData = $("form").serialize();
     }
+
+    $document.on("foundation-contentloaded", addFragmentFormTab);
 })(jQuery, jQuery(document));
