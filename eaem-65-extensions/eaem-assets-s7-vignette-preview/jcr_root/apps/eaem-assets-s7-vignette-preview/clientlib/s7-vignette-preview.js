ee71916671c12f3c5db6f60494024032f74c2168
nalabotu <nalabotu@adobe.com>
2020-08-13 10:44:02 -0500

    changes

--- eaem-65-extensions/eaem-assets-s7-vignette-preview/jcr_root/apps/eaem-assets-s7-vignette-preview/clientlib/s7-vignette-preview.js
+++ eaem-65-extensions/eaem-assets-s7-vignette-preview/jcr_root/apps/eaem-assets-s7-vignette-preview/clientlib/s7-vignette-preview.js
@@ -1,7 +1,21 @@
 (function ($, $document) {
+    var PREVIEW_IMG = "#eaem-assets-preview-colorization-img";
+
     $document.on("foundation-contentloaded", init);
 
     function init(){
-        console.log("loaded");
+        var $url =  $("[name='./url']");
+
+        $url.change(changePreview);
+    }
+
+    function changePreview(){
+        var url = this.value;
+
+        if(_.isEmpty(url)){
+            return;
+        }
+
+        $(PREVIEW_IMG).attr("src", url);
     }
 }(jQuery, jQuery(document)));
--- eaem-65-extensions/eaem-assets-s7-vignette-preview/jcr_root/apps/eaem-assets-s7-vignette-preview/preview/preview.jsp
+++ eaem-65-extensions/eaem-assets-s7-vignette-preview/jcr_root/apps/eaem-assets-s7-vignette-preview/preview/preview.jsp
@@ -1,10 +1,7 @@
 <%@ page import="com.adobe.granite.ui.components.Value" %>
 <%@include file="/libs/granite/ui/global.jsp" %>
 
-<%
-%>
-
 <div>
-    <h1>sreek</h1>
+    <img id="eaem-assets-preview-colorization-img"/>
 </div>
 
