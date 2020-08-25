e4bd483c770f21026bd5b39e2c60d150bd572662
nalabotu <nalabotu@adobe.com>
2020-08-25 16:00:33 -0500

    changes

--- eaem-65-extensions/eaem-sites-spa-dm-video-container/ui.apps/src/main/content/jcr_root/apps/eaem-sites-spa-dm-video-container/clientlibs/clientlib-dmvideo/spa-dmvideo.js
+++ eaem-65-extensions/eaem-sites-spa-dm-video-container/ui.apps/src/main/content/jcr_root/apps/eaem-sites-spa-dm-video-container/clientlibs/clientlib-dmvideo/spa-dmvideo.js
@@ -1,17 +1,76 @@
 (function ($, $document) {
-    var DM_VIDEO_FIELD = "./eaemDMVideo";
+    var DM_VIDEO_FIELD = "./eaemDMVideo",
+        ENCODE_SELECT = "./eaemDMEncode",
+        DM_VIDEO_RENDS_URL = "/apps/eaem-sites-spa-dm-video-container/video-dyn-renditions/content.html";
 
     $document.on('dialog-ready', handleVideoField);
 
     function handleVideoField(){
         var $videoField = $("foundation-autocomplete[name='" + DM_VIDEO_FIELD + "']");
 
-        $videoField.on("change", function(event){
-            var selVideoPath = event.target.value;
+        if(_.isEmpty($videoField)){
+            return;
+        }
 
-            if(_.isEmpty(selVideoPath)){
+        setSelectedEncode($videoField.val());
+
+        $videoField.on("change", addEncodes);
+    }
+
+    function addEncodes(event){
+        var $encodeField = $("coral-select[name='" + ENCODE_SELECT + "']"),
+            selVideoPath = event.target.value;
+
+        if(_.isEmpty(selVideoPath) || _.isEmpty($encodeField)){
+            return;
+        }
+
+        $encodeField.find("coral-select-item").not("[value='NONE']").remove();
+
+        loadEncodesInSelect(selVideoPath);
+    }
+
+    function getCoralSelectItem(text, value, selected){
+        return '<coral-select-item value="' + value + '" ' + selected + '>' + text + '</coral-select-item>';
+    }
+
+    function loadEncodesInSelect(videoPath, selectedValue){
+        var $encodeField = $("coral-select[name='" + ENCODE_SELECT + "']");
+
+        $.ajax(DM_VIDEO_RENDS_URL + videoPath).done(function(renditions){
+            _.each(renditions,function(rendition){
+                $encodeField.append(getCoralSelectItem(rendition.name, rendition.name,
+                                        ((selectedValue == rendition.name) ? "selected" : "")));
+            });
+        });
+    }
+
+    function setSelectedEncode(selVideoPath){
+        if(!selVideoPath){
+            return;
+        }
+
+        var dialogPath;
+
+        try{
+            dialogPath = Granite.author.DialogFrame.currentDialog.editable.slingPath;
+        }catch(err){
+            console.log("Error getting dialog path...", err);
+        }
+
+        if(!dialogPath){
+            return;
+        }
+
+        $.ajax(dialogPath).done(function(data){
+            var dmEncode = data[ENCODE_SELECT.substr(2)];
+
+            if(!dmEncode){
                 return;
             }
-        });
+
+            loadEncodesInSelect(selVideoPath, dmEncode);
+        })
+
     }
 }(jQuery, jQuery(document)));
