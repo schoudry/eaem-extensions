d2fafd34dbc499d8e558fccf265da142d25f6ea4
nalabotu <nalabotu@adobe.com>
2020-08-26 13:15:29 -0500

    changes

--- eaem-65-extensions/eaem-sites-spa-dm-video-container/core/src/main/java/com/eaem/core/models/EAEMDMVideoContainerModelExporter.java
+++ eaem-65-extensions/eaem-sites-spa-dm-video-container/core/src/main/java/com/eaem/core/models/EAEMDMVideoContainerModelExporter.java
@@ -1,10 +1,13 @@
 package com.eaem.core.models;
 
 import com.adobe.cq.export.json.ContainerExporter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * Created by nalabotu on 8/26/2020.
  */
 public interface EAEMDMVideoContainerModelExporter extends ContainerExporter {
+
+    @JsonInclude
     public String getDMVideoPath();
 }
--- eaem-65-extensions/eaem-sites-spa-dm-video-container/core/src/main/java/com/eaem/core/models/impl/EAEMDMVideoContainerModelImpl.java
+++ eaem-65-extensions/eaem-sites-spa-dm-video-container/core/src/main/java/com/eaem/core/models/impl/EAEMDMVideoContainerModelImpl.java
@@ -2,11 +2,14 @@ package com.eaem.core.models.impl;
 
 import com.adobe.cq.export.json.ContainerExporter;
 import com.day.cq.wcm.foundation.model.responsivegrid.ResponsiveGrid;
+import com.day.cq.wcm.foundation.model.responsivegrid.export.ResponsiveGridExporter;
 import com.eaem.core.models.EAEMDMVideoContainerModelExporter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.apache.sling.api.SlingHttpServletRequest;
 import com.adobe.cq.export.json.ComponentExporter;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -16,19 +19,20 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+import java.util.Map;
+
 @Model(
         adaptables = {SlingHttpServletRequest.class},
-        adapters = {ResponsiveGrid.class, ContainerExporter.class},
-        resourceType = {"eaem-sites-spa-dm-video-container/components/container"}
+        adapters = {EAEMDMVideoContainerModelImpl.class, ContainerExporter.class},
+        resourceType = {"eaem-sites-spa-dm-video-container/components/contaisner"}
 )
 @Exporter(
         name = "jackson",
         extensions = {"json"}
 )
 @JsonSerialize(as = EAEMDMVideoContainerModelExporter.class)
-public class EAEMDMVideoContainerModelImpl extends ResponsiveGrid implements EAEMDMVideoContainerModelExporter {
+public class EAEMDMVideoContainerModelImpl extends ResponsiveGrid implements EAEMDMVideoContainerModelExporter{
 
-    @JsonIgnore
     public String getDMVideoPath() {
         return "Sreek";
     }
