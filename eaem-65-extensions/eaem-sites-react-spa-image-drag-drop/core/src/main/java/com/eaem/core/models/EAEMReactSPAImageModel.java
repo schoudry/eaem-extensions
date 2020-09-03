88be09cca59b80703fc4d2a6c52ce319c57cfa3f
nalabotu <nalabotu@adobe.com>
2020-09-03 16:39:01 -0500

    changes

---
+++ eaem-65-extensions/eaem-sites-react-spa-image-drag-drop/core/src/main/java/com/eaem/core/models/EAEMReactSPAImageModel.java
@@ -0,0 +1,25 @@
+package com.eaem.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.wcm.core.components.models.Image;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.PostConstruct;
+
+@Model(
+        adaptables = {SlingHttpServletRequest.class},
+        adapters = {Image.class, ComponentExporter.class},
+        resourceType = {"eaem-sites-spa-how-to-react/components/image"}
+)
+@Exporter(
+        name = "jackson",
+        extensions = {"json"}
+)
+public class EAEMReactSPAImageModel implements Image {
+
+    @PostConstruct
+    protected void initModel() {
+    }
+}
---
+++ eaem-65-extensions/eaem-sites-react-spa-image-drag-drop/ui.frontend/src/components/Image/Image.js
@@ -0,0 +1,36 @@
+import { MapTo } from '@adobe/cq-react-editable-components';
+import DOMPurify from 'dompurify';
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+
+const ImageEditConfig = {
+    emptyLabel: 'Image - Experience AEM',
+
+    isEmpty: function (props) {
+        return (!props || !props.imageURL || (props.imageURL.trim().length < 1));
+    }
+};
+
+class Image extends Component {
+    get imageHTML() {
+        const imgStyles = {
+            "display": 'block',
+            "margin-left": 'auto',
+            "margin-right": 'auto'
+        };
+
+        return (
+            <div>
+                <Link to={this.props.imageLink}>
+                    <img src={this.props.imageURL} style={imgStyles}/>
+                </Link>
+            </div>
+        );
+    }
+
+    render() {
+        return this.imageHTML;
+    }
+}
+
+export default MapTo('eaem-sites-spa-how-to-react/components/image')(Image,ImageEditConfig);
