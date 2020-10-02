a6cac05216ec0de223f82b5d7071c0c10dc3c9bf
nalabotu <nalabotu@adobe.com>
2020-10-02 17:17:43 -0500

    changes

--- eaem-65-extensions/eaem-sites-react-spa-dm-image/ui.frontend/src/components/DMSmartCropImage/DMSmartCropImage.tsx
+++ eaem-65-extensions/eaem-sites-react-spa-dm-image/ui.frontend/src/components/DMSmartCropImage/DMSmartCropImage.tsx
@@ -1,12 +1,21 @@
 import { MapTo } from '@adobe/cq-react-editable-components';
-import DOMPurify from 'dompurify';
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import CSS from 'csstype';
 
-function isObjectEmpty(obj){
+function isObjectEmpty(obj) {
     return (Object.keys(obj).length == 0);
+}
+
+interface ImageComponentProps {
+    smartCrops: object
+    fileReference: string
+    imageLink: string
+}
+
+interface ImageComponentState {
+    imageSrc: string
 }
 
 const ImageEditConfig = {
@@ -17,34 +26,48 @@ const ImageEditConfig = {
     }
 };
 
-class Image extends Component {
-    get imageHTML() {
-        const imgStyles : CSS.Properties = {
-            marginLeft: 'auto',
-            marginRight: 'auto'
-        };
+class Image extends React.Component<ImageComponentProps, ImageComponentState> {
+    constructor(props: ImageComponentProps) {
+        super(props);
 
-        const imageProps = this.props as any;
-        let imageSrc = imageProps.fileReference;
+        this.state = {
+            imageSrc: this.imageUrl()
+        }
+    }
 
-        if(!isObjectEmpty(imageProps.smartCrops)){
-            const breakPoints = Object.keys(imageProps.smartCrops).sort((a : any,b : any) => b-a);
+    imageUrl(){
+        const imageProps = this.props as ImageComponentProps;
+        let src = imageProps.fileReference;
 
-            for(const i in breakPoints){
+        if (!isObjectEmpty(imageProps.smartCrops)) {
+            const breakPoints = Object.keys(imageProps.smartCrops).sort((a: any, b: any) => b - a);
+
+            for (const i in breakPoints) {
                 let bp = parseInt(breakPoints[i]);
 
-                if(bp < window.innerWidth){
-                    imageSrc = imageProps.smartCrops[bp];
+                if (bp < window.innerWidth) {
+                    src = imageProps.smartCrops[bp];
 
                     break;
                 }
             }
         }
 
+        return src;
+    }
+
+    get imageHTML() {
+        const imgStyles: CSS.Properties = {
+            marginLeft: 'auto',
+            marginRight: 'auto'
+        };
+
+        const imageProps = this.props as any;
+
         return (
             <div>
                 <Link to={imageProps.imageLink}>
-                    <img src={imageSrc} style={imgStyles} />
+                    <img src={this.state.imageSrc} style={imgStyles} />
                 </Link>
             </div>
         );
