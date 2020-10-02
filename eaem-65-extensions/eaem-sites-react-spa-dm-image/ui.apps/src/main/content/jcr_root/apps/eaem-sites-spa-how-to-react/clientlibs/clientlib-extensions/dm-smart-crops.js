cb33c5989be3d711de858a09c2f86f4b899862d0
nalabotu <nalabotu@adobe.com>
2020-10-02 13:40:54 -0500

    changes

--- eaem-65-extensions/eaem-sites-react-spa-dm-image/ui.apps/src/main/content/jcr_root/apps/eaem-sites-spa-how-to-react/clientlibs/clientlib-extensions/dm-smart-crops.js
+++ eaem-65-extensions/eaem-sites-react-spa-dm-image/ui.apps/src/main/content/jcr_root/apps/eaem-sites-spa-how-to-react/clientlibs/clientlib-extensions/dm-smart-crops.js
@@ -1,17 +1,52 @@
-(function($, $document){
+(function ($, $document) {
     var DM_FILE_REF = "[name='./fileReference']",
-        CROP_SELECT = "[name='./crop']",
         CROPS_MF = "[data-granite-coral-multifield-name='./crops']",
         SMART_CROPS_URL = "/apps/eaem-sites-spa-how-to-react/smart-crop-renditions/renditions.html",
         dynRenditions = {};
 
     $document.on('dialog-ready', loadSmartCrops);
 
-    function loadSmartCrops(){
-        $(CROPS_MF).on("change", function(){
+    function loadSmartCrops() {
+        var dialogPath;
+
+        try {
+            dialogPath = Granite.author.DialogFrame.currentDialog.editable.slingPath;
+        } catch (err) {
+            console.log("Error getting dialog path...", err);
+        }
+
+        if (!dialogPath) {
+            return;
+        }
+
+        dialogPath = dialogPath.substring(0, dialogPath.lastIndexOf(".json"));
+
+        $.ajax(dialogPath + ".2.json").done(handleCropsMF);
+    }
+
+    function handleCropsMF(dialogData) {
+        var $cropsMF = $(CROPS_MF),
+            mfName = $cropsMF.attr("data-granite-coral-multifield-name"),
+            selectData = dialogData[mfName.substr(2)];
+
+        $cropsMF.find("coral-select").each(function (index, cropSelect) {
+            var $cropSelect = $(cropSelect), selUrl,
+                name = $cropSelect.attr("name");
+
+            name = name.substring(mfName.length + 1);
+            name = name.substring(0,name.indexOf("/"));
+
+            if(selectData[name]){
+                selUrl = selectData[name]["url"];
+            }
+
+            loadCropsInSelect($cropSelect, selUrl);
+        });
+
+        $(CROPS_MF).on("change", function () {
             var multifield = this;
 
-            _.defer(function(){
+            _.defer(function () {
                 var justAddedItem = multifield.items.last(),
                     $cropSelect = $(justAddedItem).find("coral-select");
 
@@ -20,31 +55,31 @@
         });
     }
 
-    function getCoralSelectItem(text, value, selected){
+    function getCoralSelectItem(text, value, selected) {
         return '<coral-select-item value="' + value + '" ' + selected + '>' + text + '</coral-select-item>';
     }
 
-    function loadCropsInSelect($cropSelect, selectedValue){
+    function loadCropsInSelect($cropSelect, selectedValue) {
         var $fileRef = $(DM_FILE_REF),
             fileRef = $fileRef.val();
 
-        if(!fileRef){
+        if (!fileRef) {
             return;
         }
 
-        if(_.isEmpty(dynRenditions)){
-            $.ajax(SMART_CROPS_URL + fileRef).done(function(renditions){
+        if (_.isEmpty(dynRenditions)) {
+            $.ajax({url: SMART_CROPS_URL + fileRef, async: false}).done(function (renditions) {
                 dynRenditions = renditions;
                 addInSelect();
             });
-        }else{
+        } else {
             addInSelect();
         }
 
-        function addInSelect(){
-            _.each(dynRenditions,function(rendition){
+        function addInSelect() {
+            _.each(dynRenditions, function (rendition) {
                 $cropSelect.append(getCoralSelectItem(rendition.name, rendition.url,
-                    ((selectedValue == rendition.name) ? "selected" : "")));
+                    ((selectedValue == rendition.url) ? "selected" : "")));
             });
         }
     }
