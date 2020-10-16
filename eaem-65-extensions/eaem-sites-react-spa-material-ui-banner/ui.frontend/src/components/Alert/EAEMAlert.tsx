34fe975a520d05f3280cd5735162f2494d674c3d
nalabotu <nalabotu@adobe.com>
2020-10-16 14:02:42 -0500

    changes

--- eaem-65-extensions/eaem-sites-react-spa-material-ui-banner/ui.frontend/src/components/Alert/EAEMAlert.tsx
+++ eaem-65-extensions/eaem-sites-react-spa-material-ui-banner/ui.frontend/src/components/Alert/EAEMAlert.tsx
@@ -8,10 +8,51 @@ import {
   makeStyles,
   Theme,
   Portal,
+  SvgIcon,
+  SvgIconProps,
   Collapse
 } from "@material-ui/core";
-import { EAEMCloseIcon } from "icons";
 import CSS from "csstype";
+import classNames from "classnames";
+
+const iconStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      fontSize: 20
+    }
+  })
+);
+
+const EAEMCloseIcon: FC<SvgIconProps> = props => {
+  const classes = iconStyles();
+
+  return (
+    <SvgIcon
+      viewBox="0 0 20 20"
+      {...props}
+      className={classNames(classes.root, props.className)}
+    >
+      <title>Combined Shape</title>
+      <desc>Created with Sketch.</desc>
+      <g
+        id="Symbols"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g id="Grommet/X-Close" transform="translate(-15.000000, -15.000000)">
+          <rect id="Rectangle" x="0" y="0" width="50" height="50"></rect>
+          <path
+            d="M34.3548387,15 L35,15.6451613 L25.645,24.999 L35,34.3548387 L34.3548387,35 L25,25.645 L15.6451613,35 L15,34.3548387 L24.354,25 L15,15.6451613 L15.6451613,15 L25,24.354 L34.3548387,15 Z"
+            id="Combined-Shape"
+            fill="currentColor"
+          ></path>
+        </g>
+      </g>
+    </SvgIcon>
+  );
+};
 
 type AlertProps = {
   showAlert: string;
