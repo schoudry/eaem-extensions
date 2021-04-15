93606fc9bee2044361888b14409300efbc96f7d6
nalabotu <nalabotu@adobe.com>
2021-04-15 07:54:16 -0500

    changes

--- eaem-cloud-service/eaem-cs-sort-image-profiles/ui.apps/src/main/content/jcr_root/apps/eaem-cs-sort-image-profiles/clientlibs/sort-image-profiles/sort-profiles.js
+++ eaem-cloud-service/eaem-cs-sort-image-profiles/ui.apps/src/main/content/jcr_root/apps/eaem-cs-sort-image-profiles/clientlibs/sort-image-profiles/sort-profiles.js
@@ -11,9 +11,28 @@
             return;
         }
 
-        var items = $imageProfileSelect[0].items;
+        var selectItems = $imageProfileSelect[0].items,
+            values = selectItems.getAll(),
+            noneValue = values[0];
 
-        console.log(items);
+        values.sort(function (a, b) {
+            var nameA = a.textContent.toUpperCase();
+            var nameB = b.textContent.toUpperCase();
+
+            return ((nameA < nameB) ? -1 : ((nameA > nameB)  ? 1 : 0));
+        });
+
+        selectItems.clear();
+
+        selectItems.add(noneValue);
+
+        _.each(values, function(value){
+            if(value.textContent === "None"){
+                return;
+            }
+
+            selectItems.add(value);
+        });
     }
 
 }(jQuery, jQuery(document)));
