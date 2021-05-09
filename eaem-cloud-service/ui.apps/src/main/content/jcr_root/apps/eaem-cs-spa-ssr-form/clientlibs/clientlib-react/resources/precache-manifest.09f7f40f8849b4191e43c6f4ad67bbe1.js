0e5fd4522a4903c898dd433b3daaa5b62e71e3f9
nalabotu <nalabotu@adobe.com>
2021-05-09 07:29:15 -0500

    changes

---
+++ eaem-cloud-service/ui.apps/src/main/content/jcr_root/apps/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/precache-manifest.09f7f40f8849b4191e43c6f4ad67bbe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30bd653cad7cede6ee7ca600b62f5e9",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "653119068b96c90caf47",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/static/css/main.ba6b3b4f.chunk.css"
+  },
+  {
+    "revision": "1d622657f7dcbffddac5",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/static/js/2.496fdd1c.chunk.js"
+  },
+  {
+    "revision": "5d918e39a06717301700a001c8975e0f",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/static/js/2.496fdd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653119068b96c90caf47",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/static/js/main.44c703bb.chunk.js"
+  },
+  {
+    "revision": "05f8c7d1b159689e5214",
+    "url": "/etc.clientlibs/eaem-cs-spa-ssr-form/clientlibs/clientlib-react/resources/static/js/runtime-main.53daa9e3.js"
+  }
+]);
