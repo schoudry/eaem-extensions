9c669ef5a3837287a4d01490667efa4031b1ff91
nalabotu <nalabotu@adobe.com>
2021-05-10 15:05:40 -0500

    changes

--- eaem-cloud-service/eaem-cs-spa-read-post-data/ui.frontend/src/components/Steps/Steps.tsx
+++ eaem-cloud-service/eaem-cs-spa-read-post-data/ui.frontend/src/components/Steps/Steps.tsx
@@ -1,5 +1,10 @@
 import { MapTo } from "@adobe/aem-react-editable-components";
 import React, { FC, useState, useEffect } from "react";
+import "./StepsStyles.css";
+
+type StepsProps = {
+    [x: string]: any
+};
 
 const StepsEditConfig = {
     emptyLabel: "Steps - Experience AEM",
@@ -9,13 +14,43 @@ const StepsEditConfig = {
     }
 };
 
-const AEMSteps: FC = props => {
+const AEMSteps: FC<StepsProps> = props => {
     return (
-      <div>
-           This is sreekanth 
-      </div>
+        <div>
+            <h1 style={{ textAlign: "center", color: "maroon" }}>
+                {props["jcr:title"]}
+            </h1>
+
+            {
+                props.showName &&
+                <div className='eaem-info'>
+                    <span>Enter name</span>
+                    <input name='eaemName'></input>
+                </div>
+            }
+            {
+                props.showEmail &&
+                <div className='eaem-info'>
+                    <span>Enter email</span>
+                    <input name='eaemEmail'></input>
+                </div>
+            }
+            {
+                props.showSSN &&
+                <div className='eaem-info'>
+                    <span>Enter SSN</span>
+                    <input name='eaemSSN'></input>
+                </div>
+            }
+            {
+                props.showCompany &&
+                <div className='eaem-info'>
+                    <span>Enter company</span>
+                    <input name='eaemCompany'></input>
+                </div>
+            }
+        </div>
     );
-  };
-  
-  export default MapTo("eaem-cs-spa-read-post-data/components/text")(AEMSteps, StepsEditConfig);
-  
+};
+
+export default MapTo("eaem-cs-spa-read-post-data/components/steps")(AEMSteps, StepsEditConfig);
