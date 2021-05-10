fd48949a76bdaae38678ab478b267ac458eeebbb
nalabotu <nalabotu@adobe.com>
2021-05-10 17:34:29 -0500

    changes

---
+++ eaem-cloud-service/eaem-cs-spa-read-post-data/core/src/main/java/apps/experienceaem/sites/spa/core/filters/OnBoardingPostModifierFilter.java
@@ -0,0 +1,63 @@
+package experienceaem.sites.spa.core.filters;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component(
+        service = Filter.class,
+        immediate = true,
+        name = "Experience AEM folder nesting check",
+        property = {
+                Constants.SERVICE_RANKING + ":Integer=-99",
+                "sling.filter.scope=COMPONENT",
+                "sling.filter.pattern=(/content/eaem-cs-spa-read-post-data/us/en/home*)",
+        }
+)
+public class OnBoardingPostModifierFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(OnBoardingPostModifierFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+
+        try {
+            if(!slingRequest.getMethod().equals("POST")){
+                chain.doFilter(request, response);
+                return;
+            }
+
+            RequestDispatcher dp = request.getRequestDispatcher(slingRequest.getRequestPathInfo().getResourcePath() + ".html");
+
+            dp.include(new GetSlingServletRequestWrapper(slingRequest), response);
+        } catch (Exception e) {
+            log.error("Error converting POST to get of SPA home : " + slingRequest.getRequestURI());
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    private class GetSlingServletRequestWrapper extends SlingHttpServletRequestWrapper {
+        public GetSlingServletRequestWrapper(final SlingHttpServletRequest request) {
+            super(request);
+        }
+
+        public String getMethod() {
+            return "GET";
+        }
+    }
+}
