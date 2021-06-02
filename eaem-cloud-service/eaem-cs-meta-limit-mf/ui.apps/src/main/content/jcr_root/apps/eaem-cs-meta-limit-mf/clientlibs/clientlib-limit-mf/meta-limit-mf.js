078734e626eab5df89f117103f8db125c0d8b6b2
nalabotu <nalabotu@adobe.com>
2021-06-02 08:27:53 -0500

    mf validation

--- eaem-cloud-service/eaem-cs-meta-limit-mf/ui.apps/src/main/content/jcr_root/apps/eaem-cs-meta-limit-mf/clientlibs/clientlib-limit-mf/meta-limit-mf.js
+++ eaem-cloud-service/eaem-cs-meta-limit-mf/ui.apps/src/main/content/jcr_root/apps/eaem-cs-meta-limit-mf/clientlibs/clientlib-limit-mf/meta-limit-mf.js
@@ -1,13 +1,33 @@
 (function($, $document){
     var DATA_MF_NAME = "data-granite-coral-multifield-name",
         MF_NAME = "./jcr:content/metadata/eaemKeywords",
+        EAEM_VALIDATION_ERROR = "eaem-assets-validation-error",
         EAEM_MAX_ITEMS = 5,
-        EAEM_MIN_ITEMS = 1;
+        EAEM_MIN_ITEMS = 1, continueSave = true;
 
     $document.on("foundation-contentloaded", validateMF);
 
     function validateMF(){
-        addValidator($("[" + DATA_MF_NAME + "='" + MF_NAME + "']"), EAEM_MAX_ITEMS, EAEM_MIN_ITEMS);
+        var $multifield = $("[" + DATA_MF_NAME + "='" + MF_NAME + "']");
+
+        addValidator($multifield, EAEM_MAX_ITEMS, EAEM_MIN_ITEMS);
+
+        var validation = $multifield.adaptTo("foundation-validation");
+
+        validation.checkValidity();
+
+        validation.updateUI();
+
+        $("#shell-propertiespage-doneactivator").click(function(event){
+            if(continueSave){
+                return;
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            showAlert("error", "Error", "Validation failed");
+        })
     }
 
     function addValidator($multifield, maxItems, minItems){
@@ -19,7 +39,11 @@
             minItems = parseInt(minItems);
         }
 
-        $.validator.register({
+        $multifield.attr("aria-required","true");
+
+        var registry = $(window).adaptTo("foundation-registry");
+
+        registry.register("foundation.validation.validator", {
             selector: "#" + $multifield.attr("id"),
             validate: validate
         });
@@ -27,15 +51,47 @@
         function validate(){
             var count = $multifield[0]._items.length;
 
+            continueSave = true;
+
             if(maxItems && (count > maxItems) ){
+                continueSave = false;
                 return "Maximum allowed : " + maxItems + " items";
             }
 
             if(minItems && (count < minItems) ){
+                continueSave = false;
                 return "Minimum required : " + minItems + " items";
             }
 
-            return null;
+            return false;
         }
+    }
+
+    function showAlert(variant, header, content) {
+        var $dialog = $("#" + EAEM_VALIDATION_ERROR), dialog;
+
+        if (!_.isEmpty($dialog)) {
+            $dialog[0].show();
+            return;
+        }
+
+        dialog = new Coral.Dialog().set({
+            id: EAEM_VALIDATION_ERROR,
+            variant: variant,
+            closable: "on",
+            header: {
+                innerHTML: header
+            },
+            content: {
+                innerHTML: content
+            },
+            footer: {
+                innerHTML: '<button is="coral-button" variant="default" coral-close>OK</button>'
+            }
+        });
+
+        document.body.appendChild(dialog);
+
+        dialog.show();
     }
 }(jQuery, jQuery(document)));
