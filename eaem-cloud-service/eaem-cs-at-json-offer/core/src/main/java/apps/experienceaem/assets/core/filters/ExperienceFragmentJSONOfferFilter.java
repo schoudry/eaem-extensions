f792d3f82ea36702bf7c9eba060f30a9ac104c2d
nalabotu <nalabotu@adobe.com>
2021-06-14 11:41:05 -0500

    changes

---
+++ eaem-cloud-service/eaem-cs-at-json-offer/core/src/main/java/apps/experienceaem/assets/core/filters/ExperienceFragmentJSONOfferFilter.java
@@ -0,0 +1,60 @@
+package apps.experienceaem.assets.core.filters;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.commons.json.JSONObject;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component(
+        service = Filter.class,
+        immediate = true,
+        name = "Experience AEM - Change offer JSON exported to Target",
+        property = {
+                Constants.SERVICE_RANKING + ":Integer=-99",
+                "sling.filter.scope=COMPONENT",
+                "sling.filter.pattern=(/content/experience-fragments/.*.model.json)",
+        }
+)
+public class ExperienceFragmentJSONOfferFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(ExperienceFragmentJSONOfferFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+
+        try {
+            String uri = slingRequest.getRequestURI();
+
+            if(!uri.endsWith(".model.json")){
+                chain.doFilter(request, response);
+                return;
+            }
+
+            JSONObject model = new JSONObject();
+
+            String masterXFPath = uri.substring(0,uri.lastIndexOf(".model.json"));
+
+            masterXFPath = masterXFPath + ".html";
+
+            model.put("xfHtmlPath", masterXFPath);
+
+            response.getWriter().print(model);
+        } catch (Exception e) {
+            log.error("Error getting json offer response : " + slingRequest.getRequestURI());
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
