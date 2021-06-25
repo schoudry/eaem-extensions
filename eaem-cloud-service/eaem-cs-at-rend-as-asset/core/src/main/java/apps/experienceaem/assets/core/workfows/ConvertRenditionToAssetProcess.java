6757e9e047e48b4c8ebd3459f7c4f19b68200600
nalabotu <nalabotu@adobe.com>
2021-06-25 06:52:02 -0500

    changes

--- eaem-cloud-service/eaem-cs-at-rend-as-asset/core/src/main/java/apps/experienceaem/assets/core/workfows/ConvertRenditionToAssetProcess.java
+++ eaem-cloud-service/eaem-cs-at-rend-as-asset/core/src/main/java/apps/experienceaem/assets/core/workfows/ConvertRenditionToAssetProcess.java
@@ -7,6 +7,7 @@ import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowData;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.day.cq.contentsync.handler.util.RequestResponseFactory;
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.AssetManager;
 import com.day.cq.dam.api.Rendition;
@@ -14,15 +15,18 @@ import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.commons.mime.MimeTypeService;
+import org.apache.sling.engine.SlingRequestProcessor;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jcr.Binary;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 @Component(
         service = WorkflowProcess.class,
@@ -34,11 +38,20 @@ public class ConvertRenditionToAssetProcess implements WorkflowProcess {
     private static final String RELATION_SOURCES = "sources";
     private static final String RELATION_DERIVED = "derived";
     private static final String DAM_DERIVED_ASSET = "dam:derivedAsset";
+    private static final String PROCESS_ASSET_SERVLET = "/bin/asynccommand";
+    private static final String PROCESS_ASSET_OPERATION = "PROCESS";
+    private static final String PROCESS_ASSET_PROFILE = "full-process";
 
     private static String FIGMA_REND_NAME = "figma";
 
     @Reference
     private MimeTypeService mimeTypeService;
+
+    @Reference
+    private SlingRequestProcessor slingRequestProcessor;
+
+    @Reference
+    private RequestResponseFactory requestResponseFactory;
 
     public void execute(final WorkItem workItem, final WorkflowSession workflowSession, final MetaDataMap args)
             throws WorkflowException {
@@ -70,34 +83,65 @@ public class ConvertRenditionToAssetProcess implements WorkflowProcess {
                 return;
             }
 
-            String newAssetPath = createAssetPath(assetRes.getParent(), assetNameNoExt, rendition.getName());
+            Asset derivedAsset = createFigmaAsset(resolver, assetRes, rendition);
 
-            String mimeType = mimeTypeService.getMimeType(rendition.getName());
-
-            AssetManager assetManager = resolver.adaptTo(AssetManager.class);
-
-            Asset derivedAsset = assetManager.createOrReplaceAsset(newAssetPath, rendition.getBinary(), mimeType, false);
-
-            if (derivedAsset == null) {
+            if(derivedAsset == null){
                 log.warn("Error creating Figma asset for : " + assetPath);
                 return;
             }
 
-            addRelation(derivedAsset, RELATION_SOURCES, asset.getPath());
-
-            addRelation(asset, RELATION_DERIVED, newAssetPath);
-
-            Resource derivedAssetResource = derivedAsset.adaptTo(Resource.class);
-
-            ModifiableValueMap valueMap = derivedAssetResource.adaptTo(ModifiableValueMap.class);
-
-            valueMap.put(DAM_DERIVED_ASSET, true);
-
             resolver.commit();
+
+            processNewAsset(resolver, derivedAsset.getPath());
         }catch(Exception e){
             log.error("Error occured while converting rendtion to asset for payload - " + assetPath, e);
         }
     }
+
+    private Asset createFigmaAsset(ResourceResolver resolver, Resource assetRes, Rendition rendition){
+        Asset asset = assetRes.adaptTo(Asset.class);
+
+        String assetNameNoExt = assetRes.getName().substring(0, assetRes.getName().lastIndexOf("."));
+
+        String newAssetPath = createAssetPath(assetRes.getParent(), assetNameNoExt, rendition.getName());
+
+        String mimeType = mimeTypeService.getMimeType(rendition.getName());
+
+        AssetManager assetManager = resolver.adaptTo(AssetManager.class);
+
+        Asset derivedAsset = assetManager.createOrReplaceAsset(newAssetPath, rendition.getBinary(), mimeType, false);
+
+        if (derivedAsset == null) {
+            return null;
+        }
+
+        addRelation(derivedAsset, RELATION_SOURCES, asset.getPath());
+
+        addRelation(asset, RELATION_DERIVED, newAssetPath);
+
+        return  derivedAsset;
+    }
+
+    private void processNewAsset(ResourceResolver resolver, String assetPath){
+        Map<String, Object> requestParams = new HashMap<String, Object>();
+        requestParams.put("operation", PROCESS_ASSET_OPERATION);
+        requestParams.put("profile-select", PROCESS_ASSET_PROFILE);
+        requestParams.put("runPostProcess", "false"); // donot run the post process, might resulting in a processing loop
+        requestParams.put("description", "Processing created figma asset - " + assetPath);
+        requestParams.put("asset", assetPath);
+
+        HttpServletRequest request = requestResponseFactory.createRequest("POST", PROCESS_ASSET_SERVLET, requestParams);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        HttpServletResponse response = this.requestResponseFactory.createResponse(bos);
+
+        try {
+            slingRequestProcessor.processRequest(request, response, resolver);
+        }catch (Exception e){
+            log.error("Error occured while processing the new figma asset - " + assetPath);
+        }
+    }
+
 
     private void addRelation(Asset asset, String name, String relatedAssetPath) {
         com.adobe.granite.asset.api.Asset graniteAsset = asset.adaptTo(com.adobe.granite.asset.api.Asset.class);
