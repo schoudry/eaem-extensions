c7a0eb63dec29ba360350dd1561967465eaf1b59
nalabotu <nalabotu@adobe.com>
2021-09-10 14:38:38 -0500

    changes

--- eaem-65-extensions/eaem-migrate-smart-crops/core/src/main/java/apps/experienceaem/assets/core/workflow/MigrateSmartCropsStep.java
+++ eaem-65-extensions/eaem-migrate-smart-crops/core/src/main/java/apps/experienceaem/assets/core/workflow/MigrateSmartCropsStep.java
@@ -14,6 +14,7 @@ import com.day.cq.dam.scene7.api.S7ConfigResolver;
 import com.day.cq.dam.scene7.api.Scene7APIClient;
 import com.day.cq.dam.scene7.api.Scene7EndpointsManager;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -22,6 +23,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Component;
@@ -29,8 +31,11 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.scene7.ipsapi.*;
+import com.day.cq.dam.api.Asset;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -43,6 +48,7 @@ import javax.xml.xpath.XPathFactory;
 import java.io.ByteArrayInputStream;
 import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @Component(
@@ -56,6 +62,8 @@ public class MigrateSmartCropsStep implements WorkflowProcess {
     private static String SRC_S7_ACCT = "srcS7Account";
 
     private static String META_S7_FOLDER = "dam:scene7Folder";
+
+    private static String META_S7_ID = "dam:scene7ID";
 
     @Reference
     private Scene7EndpointsManager scene7EndpointsManager;
@@ -84,42 +92,232 @@ public class MigrateSmartCropsStep implements WorkflowProcess {
         String assetPath = getPayloadPath(workItem.getWorkflowData());
 
         try{
-            MetaDataMap wfData = workItem.getWorkflow().getMetaDataMap();
+            log.debug("Migrating smart crops for asset : " + assetPath);
 
-            setSourceS7CompanyProperties(wfData.get(SRC_S7_ACCT, String.class));
+            ResourceResolver resolver = workflowSession.adaptTo(ResourceResolver.class);
 
-            final ResourceResolver s7ConfigResourceResolver = getServiceResourceResolver(resourceResolverFactory);
-            S7Config s7Config = s7ConfigResolver.getS7ConfigForAssetPath(s7ConfigResourceResolver, assetPath);
+            Resource resource = resolver.getResource(assetPath);
+
+            if(resource == null){
+                throw new RuntimeException("Asset not found : " + assetPath);
+            }
+
+            Asset destAsset = resource.adaptTo(Asset.class);
+
+            String destScene7FolderPath = destAsset.getMetadataValue(META_S7_FOLDER);
+
+            String destAssetHandle = destAsset.getMetadataValue(META_S7_ID);
+
+            if(StringUtils.isEmpty(destScene7FolderPath)){
+                throw new RuntimeException("dam:scene7Folder not available in metadata for : " + assetPath);
+            }
+
+            if (!args.containsKey("PROCESS_ARGS")){
+                throw new RuntimeException("S7 Source account not set in workflow model or not in expected format srcS7Account=s7CompanyHandle/user/pass");
+            }
+
+            String srcS7Account = args.get("PROCESS_ARGS",String.class);
+
+            srcS7Account = srcS7Account.substring(srcS7Account.indexOf("=") + 1);
+
+            S7Config s7Config = s7ConfigResolver.getS7ConfigForAssetPath(resolver, assetPath);
 
             if (s7Config == null) {
-                s7Config = s7ConfigResolver.getDefaultS7Config(s7ConfigResourceResolver);
+                s7Config = s7ConfigResolver.getDefaultS7Config(resolver);
             }
-            log.info("Migrating smart crops for asset : " + assetPath);
+
+            setSourceS7CompanyProperties(srcS7Account);
+
+            Map<String, Map<String, String>> cropRects = getSourceS7AssetCropRect(s7Config, destScene7FolderPath, assetPath);
+
+            Map<String, String> subAssetHandles = getDestSubAssetHandles(s7Config, destAssetHandle);
+
+            updateDestS7SmartCrops(s7Config, subAssetHandles, destAssetHandle, cropRects);
+
+            log.debug("Migration complete for smart crops of asset : " + assetPath);
 
         }catch(Exception e){
             log.error("Error occured while updating crops for payload - " + assetPath, e);
         }
     }
 
-    private void getSourceS7AssetCropRect(S7Config s7Config, String assetPath) throws Exception{
-        String s7FolderPath = getSrcS7CompanyRootFolderName(s7Config);
+    private void updateDestS7SmartCrops(S7Config s7Config, Map<String, String> subAssetHandles, String assetHandle,
+                                        Map<String, Map<String, String>> cropsToUpdate) throws Exception{
+        UpdateSmartCropsParam updateSmartCropsParam = new UpdateSmartCropsParam();
+        updateSmartCropsParam.setCompanyHandle(s7Config.getCompanyHandle());
 
-        SearchAssetsParam searchAssetsParam = getSrcSearchAssetsParam(s7FolderPath);
+        SmartCropUpdateArray updateArray = new SmartCropUpdateArray();
+        SmartCropUpdate smartCropUpdate = null;
+        NormalizedCropRect cropRect = null;
+
+        Iterator<Map.Entry<String, String>> subAssetHandlesItr =  subAssetHandles.entrySet().iterator();
+        Map.Entry<String, String> entry;
+        Map<String, String> boundaries;
+
+        while(subAssetHandlesItr.hasNext()){
+            entry = subAssetHandlesItr.next();
+
+            smartCropUpdate = new SmartCropUpdate();
+            smartCropUpdate.setOwnerHandle(assetHandle);
+            smartCropUpdate.setSubAssetHandle(entry.getValue());
+
+            cropRect = new NormalizedCropRect();
+            boundaries = cropsToUpdate.get(entry.getKey());
+
+            cropRect.setLeftN(Double.parseDouble(boundaries.get("leftN")));
+            cropRect.setTopN(Double.parseDouble(boundaries.get("topN")));
+            cropRect.setWidthN(Double.parseDouble(boundaries.get("widthN")));
+            cropRect.setHeightN(Double.parseDouble(boundaries.get("heightN")));
+
+            smartCropUpdate.setCropRect(cropRect);
+            updateArray.getItems().add(smartCropUpdate);
+        }
+
+        updateSmartCropsParam.setUpdateArray(updateArray);
+
+        String responseBody = makeS7Request(s7Config, getDestS7AuthHeader(s7Config), updateSmartCropsParam);
+
+        int totalUpdated = Integer.parseInt(getSingleElementTextContent(responseBody, "/updateSmartCropsReturn/successCount"));
+
+        if(updateArray.getItems().size() != totalUpdated){
+            throw new Exception("Updated count : " + totalUpdated + ", doesnt match total count : " + updateArray.getItems().size());
+        }
+    }
+
+    public Map<String, String> getDestSubAssetHandles(S7Config s7Config, String assetHandle) throws Exception{
+        GetAssociatedAssetsParam getAssociatedAssetsParam = new GetAssociatedAssetsParam();
+        getAssociatedAssetsParam.setCompanyHandle(s7Config.getCompanyHandle());
+        getAssociatedAssetsParam.setAssetHandle(assetHandle);
+
+        String responseBody = makeS7Request(s7Config, getDestS7AuthHeader(s7Config), getAssociatedAssetsParam);
+
+        Map<String, String> subAssetHandles = new HashMap<String, String>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        ByteArrayInputStream input =  new ByteArrayInputStream(responseBody.getBytes());
+
+        Document doc = builder.parse(input);
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression = "/getAssociatedAssetsReturn/subAssetArray/items";
+
+        NodeList itemList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+        String subAssetHandle = null, name, cropRectStr;
+
+        for (int i = 0; i < itemList.getLength(); i++) {
+            Node item = itemList.item(i);
+
+            if(item.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) item;
+
+                subAssetHandle = eElement.getElementsByTagName("subAssetHandle").item(0).getTextContent();
+                name = eElement.getElementsByTagName("name").item(0).getTextContent();
+
+                subAssetHandles.put(name, subAssetHandle);
+            }
+        }
+
+        return subAssetHandles;
+    }
+
+    private Map<String, Map<String, String>> getSourceS7AssetCropRect(S7Config s7Config, String destScene7FolderPath, String assetPath) throws Exception{
+        String srcS7FolderPath = getSrcS7CompanyRootFolderName(s7Config) + destScene7FolderPath.substring(destScene7FolderPath.indexOf("/"));
+
+        String assetName = assetPath.substring(assetPath.lastIndexOf("/") + 1);
+
+        SearchAssetsByMetadataParam searchAssetsParam = getSrcSearchAssetParam(srcS7FolderPath, assetName);
 
         String responseBody = makeS7Request(s7Config, getSrcS7AuthHeader(), searchAssetsParam);
 
-        parseSearchResponse(responseBody, s7Folder);
+        String srcS7AssetHandle = getSingleElementTextContent(responseBody, "/searchAssetsByMetadataReturn/assetSummaryArray/items/assetHandle");
+
+        GetAssociatedAssetsParam getAssociatedAssetsParam = new GetAssociatedAssetsParam();
+        getAssociatedAssetsParam.setCompanyHandle(srcS7Company);
+        getAssociatedAssetsParam.setAssetHandle(srcS7AssetHandle);
+
+        responseBody = makeS7Request(s7Config, getSrcS7AuthHeader(), getAssociatedAssetsParam);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document doc = builder.parse(new ByteArrayInputStream(responseBody.getBytes()));
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression = "/getAssociatedAssetsReturn/subAssetArray/items";
+
+        NodeList itemList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+        Map<String, Map<String, String>> cropRects = new HashMap<String, Map<String, String>>();
+        Map<String, String> cropRect = null;
+
+        for (int i = 0; i < itemList.getLength(); i++) {
+            Node item = itemList.item(i);
+
+            if(item.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+
+            Element eElement = (Element) item;
+
+            String name = eElement.getElementsByTagName("name").item(0).getTextContent();
+
+            cropRect = new HashMap<String, String>();
+
+            try{
+                Element cropRectEle = (Element)((Element)eElement.getElementsByTagName("smartCrop").item(0))
+                        .getElementsByTagName("cropRect").item(0);
+
+                cropRect.put("leftN", cropRectEle.getElementsByTagName("leftN").item(0).getTextContent());
+                cropRect.put("topN", cropRectEle.getElementsByTagName("topN").item(0).getTextContent());
+                cropRect.put("widthN", cropRectEle.getElementsByTagName("widthN").item(0).getTextContent());
+                cropRect.put("heightN", cropRectEle.getElementsByTagName("heightN").item(0).getTextContent());
+            }catch(Exception ce){
+                //ignore no smart crop images
+            }
+
+            cropRects.put(name, cropRect);
+        }
+
+        return cropRects;
     }
 
-    private SearchAssetsParam getSrcSearchAssetsParam(String s7FolderPath){
-        SearchAssetsParam searchAssetsParam = new SearchAssetsParam();
-        StringArray assetTypes = new StringArray();
-        assetTypes.getItems().add("Image");
+    private String getSingleElementTextContent(String responseBody, String expression) throws Exception{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document doc = builder.parse(new ByteArrayInputStream(responseBody.getBytes()));
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        Element element = (Element) xPath.compile(expression).evaluate(doc, XPathConstants.NODE);
+
+        return element.getTextContent();
+    }
+
+    private SearchAssetsByMetadataParam getSrcSearchAssetParam(String s7FolderPath, String assetName){
+        SearchAssetsByMetadataParam searchAssetsParam = new SearchAssetsByMetadataParam();
+        MetadataConditionArray metaCondArray = new MetadataConditionArray();
+
+        MetadataCondition condition = new MetadataCondition();
+        condition.setFieldHandle("file_name");
+        condition.setOp("Equals");
+        condition.setValue(assetName);
+
+        metaCondArray.getItems().add(condition);
+
+        condition = new MetadataCondition();
+        condition.setFieldHandle("folder_path");
+        condition.setOp("Contains");
+        condition.setValue(s7FolderPath);
+
+        metaCondArray.getItems().add(condition);
 
         searchAssetsParam.setCompanyHandle(srcS7Company);
-        searchAssetsParam.setFolder(s7FolderPath);
-        searchAssetsParam.setIncludeSubfolders(false);
-        searchAssetsParam.setAssetTypeArray(assetTypes);
+        searchAssetsParam.setMetadataConditionArray(metaCondArray);
 
         return searchAssetsParam;
     }
@@ -130,18 +328,7 @@ public class MigrateSmartCropsStep implements WorkflowProcess {
 
         String responseBody = makeS7Request(s7Config, getSrcS7AuthHeader(), getCompanyInfoParam);
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document doc = builder.parse(responseBody);
-
-        XPath xPath =  XPathFactory.newInstance().newXPath();
-
-        String expression = "/getCompanyInfoReturn/companyInfo/name";
-
-        Element element = (Element) xPath.compile(expression).evaluate(doc, XPathConstants.NODE);
-
-        return element.getTextContent();
+        return getSingleElementTextContent(responseBody, "/getCompanyInfoReturn/companyInfo/rootPath");
     }
 
     private void setSourceS7CompanyProperties(String srcAccount){
@@ -165,24 +352,6 @@ public class MigrateSmartCropsStep implements WorkflowProcess {
         }
 
         return payloadPath;
-    }
-
-    public Map<String, String> getSmartCropsSubAssetHandles(S7Config s7Config, String assetHandle){
-        Map<String, String> subAssetHandles = new HashMap<String, String>();
-
-        try{
-            GetAssociatedAssetsParam getAssociatedAssetsParam = new GetAssociatedAssetsParam();
-            getAssociatedAssetsParam.setCompanyHandle(s7Config.getCompanyHandle());
-            getAssociatedAssetsParam.setAssetHandle(assetHandle);
-
-            AuthHeader authHeader = getDestS7AuthHeader(s7Config);
-
-            String responseBody = makeS7Request(s7Config, authHeader, getAssociatedAssetsParam);
-        }catch(Exception e){
-            log.error("Error getting smart crop handles for : " + assetHandle, e);
-        }
-
-        return subAssetHandles;
     }
 
     private String makeS7Request(S7Config s7Config, AuthHeader authHeader, Object param) throws Exception{
