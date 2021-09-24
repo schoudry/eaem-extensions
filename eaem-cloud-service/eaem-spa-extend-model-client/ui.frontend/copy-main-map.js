52efe7097e10aa71300966cf004852cb9a4f4c7e
nalabotu <nalabotu@adobe.com>
2021-09-24 11:36:02 -0500

    changes

--- eaem-cloud-service/eaem-spa-extend-model-client/ui.frontend/copy-main-map.js
+++ eaem-cloud-service/eaem-spa-extend-model-client/ui.frontend/copy-main-map.js
@@ -1,11 +1,16 @@
+//this is for chaning the sourceMappingURL in clientlib-react main file
 const path = require('path');
 const fs = require("fs-extra");
+const readline = require('readline');
+
+const PROJECT_FOLDER_NAME = path.basename(path.join(__dirname, '..'));
 
 const MAP_BUILD_DIR = path.join(__dirname, 'build', "static", "js");
 
 const CLIENTLIB_REACT_JS_DIR = path.join(__dirname,'..', 'ui.apps','src','main','content', 
-                            'jcr_root','apps','eaem-spa-extend-model-client','clientlibs', 'clientlib-react', "js");
+                            'jcr_root','apps', PROJECT_FOLDER_NAME ,'clientlibs', 'clientlib-react', "js");
 
+/*
 fs.copySync(MAP_BUILD_DIR, CLIENTLIB_REACT_JS_DIR, {
     dereference: true,
     filter: (filePath) => {
@@ -14,4 +19,34 @@ fs.copySync(MAP_BUILD_DIR, CLIENTLIB_REACT_JS_DIR, {
         }
         return path.basename(filePath).startsWith("main.") && path.basename(filePath).endsWith(".map");
     }
-});
+});*/
+
+const mainFile = path.join(CLIENTLIB_REACT_JS_DIR, fs.readdirSync(CLIENTLIB_REACT_JS_DIR).filter((fileName) => {
+    return fileName.startsWith("main.")
+})[0]);
+
+const mainMapPath = "/apps/" + PROJECT_FOLDER_NAME + "/clientlibs/clientlib-react/resources/static/js/" + path.basename(mainFile) + ".map";
+
+async function writeCorrectSourcePath() {
+    const fileStream = fs.createReadStream(mainFile);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    
+    let mainFileContent = "";
+
+    for await (const line of rl) {
+        if(line.startsWith("//# sourceMappingURL=")){
+            mainFileContent = mainFileContent + "//# sourceMappingURL=" + mainMapPath + "\r\n";
+            continue;
+        }            
+
+        mainFileContent = mainFileContent + line + "\r\n";
+    }
+
+    fs.writeFileSync(mainFile, mainFileContent, 'utf-8');
+}
+
+writeCorrectSourcePath();
