fc56734987cf70ef23005cda0d55ee63154ea9fb
nalabotu <nalabotu@adobe.com>
2021-09-26 16:47:20 -0500

    changes

--- eaem-cloud-service/eaem-spa-vanity-urls/ui.frontend/src/components/import-components.js
+++ eaem-cloud-service/eaem-spa-vanity-urls/ui.frontend/src/components/import-components.js
@@ -18,12 +18,65 @@ import withAsyncImport from "../utils/withAsyncImport";
 import './Page/Page';
 import './Container/Container';
 import './ExperienceFragment/ExperienceFragment';
-import './Nav/Nav';
 
 import {MapTo} from '@adobe/aem-react-editable-components';
 
+import {
+    CarouselV1IsEmptyFn
+} from '@adobe/aem-core-components-react-spa/dist/isEmptyFunctions';
+
+import {
+    TitleV2IsEmptyFn
+} from '@adobe/aem-core-components-react-base/dist/isEmptyFunctions';
+
+import {
+    ContainerV1, ContainerV1IsEmptyFn,
+    TabsV1, TabsV1IsEmptyFn,
+    AccordionV1,AccordionV1IsEmptyFn,
+} from '@adobe/aem-core-components-react-spa';
+
+import {
+    BreadCrumbV2,BreadCrumbV2IsEmptyFn,
+    ButtonV1,ButtonV1IsEmptyFn,
+    ImageV2,ImageV2IsEmptyFn,
+    LanguageNavigationV1,
+    NavigationV1,
+    TeaserV1,TeaserV1IsEmptyFn,
+    DownloadV1,DownloadV1IsEmptyFn,
+    SeparatorV1,SeparatorV1IsEmptyFn,
+    ListV2,ListV2IsEmptyFn
+} from '@adobe/aem-core-components-react-base';
+
 //lazyload / code splitting example of an internal component
 const LazyTextComponent = withAsyncImport(() => import(`./Text/Text`));
+
+//lazyload / code splitting examples of external components
+const TitleV2 = withAsyncImport(() => import(`@adobe/aem-core-components-react-base/dist/authoring/title/v2/TitleV2`));
+const CarouselV1 = withAsyncImport(() => import(`@adobe/aem-core-components-react-spa/dist/container/carousel/v1/CarouselV1`));
+
+
+MapTo('eaem-spa-vanity-urls/components/download')(DownloadV1, {isEmpty: DownloadV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/list')(ListV2, {isEmpty: ListV2IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/separator')(SeparatorV1, {isEmpty: SeparatorV1IsEmptyFn});
+
+MapTo('eaem-spa-vanity-urls/components/button')(ButtonV1, {isEmpty: ButtonV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/teaser')(TeaserV1, {isEmpty: TeaserV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/image')(ImageV2, {isEmpty: ImageV2IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/title')(TitleV2, {isEmpty: TitleV2IsEmptyFn});
+
+
+MapTo('eaem-spa-vanity-urls/components/breadcrumb')(BreadCrumbV2, {isEmpty: BreadCrumbV2IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/navigation')(NavigationV1);
+MapTo('eaem-spa-vanity-urls/components/languagenavigation')(LanguageNavigationV1);
+
+
+MapTo('eaem-spa-vanity-urls/components/tabs')(TabsV1, {isEmpty: TabsV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/accordion')(AccordionV1, {isEmpty: AccordionV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/carousel')(CarouselV1, {isEmpty: CarouselV1IsEmptyFn});
+MapTo('eaem-spa-vanity-urls/components/container')(ContainerV1, {isEmpty: ContainerV1IsEmptyFn});
+
+
+//lazy load of internal component (hello world)
 
 /**
  * Default Edit configuration for the Text component that interact with the Core Text component and sub-types
