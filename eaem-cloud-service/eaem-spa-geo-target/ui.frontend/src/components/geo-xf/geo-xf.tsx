e0e27c1ccc326e71519e9f25f088e244816d1bdb
nalabotu <nalabotu@adobe.com>
2021-10-13 12:28:30 -0500

    changes

---
+++ eaem-cloud-service/eaem-spa-geo-target/ui.frontend/src/components/geo-xf/geo-xf.tsx
@@ -0,0 +1,16 @@
+import { MapTo } from "@adobe/aem-react-editable-components";
+import React, { FC, useState, useEffect } from "react";
+
+const GeoXFConfig = {
+    emptyLabel: "Geo XF - Experience AEM",
+ 
+    isEmpty: function (props: any) {
+        return !props;
+    }
+};
+
+const GeoXF: FC = props => {
+    return <div>This is Xf Content</div>;
+}    
+
+export default MapTo('eaem-spa-geo-target/components/geo-xf')(GeoXF, GeoXFConfig);
