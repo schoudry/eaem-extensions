2204c2a020fa6ccd59b686cf13195ecb55117ca2
nalabotu <nalabotu@adobe.com>
2021-10-20 19:12:54 -0500

    changes

---
+++ eaem-cloud-service/eaem-spa-short-urls/ui.frontend/src/index.js
@@ -0,0 +1,44 @@
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9';
+import 'custom-event-polyfill';
+
+import { Constants, ModelManager } from '@adobe/aem-spa-page-model-manager';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router } from 'react-router-dom';
+import App from './App';
+import LocalDevModelClient from './LocalDevModelClient';
+import './components/import-components';
+import './index.css';
+
+const modelManagerOptions = {};
+if(process.env.REACT_APP_PROXY_ENABLED) {
+    modelManagerOptions.modelClient = new LocalDevModelClient(process.env.REACT_APP_API_HOST);
+}
+
+const renderApp = () => {
+    ModelManager.initialize(modelManagerOptions).then(pageModel => {
+        const history = createBrowserHistory();
+        render(
+            <Router history={history}>
+                <App
+                    history={history}
+                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
+                    cqItems={pageModel[Constants.ITEMS_PROP]}
+                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
+                    cqPath={pageModel[Constants.PATH_PROP]}
+                    locationPathname={window.location.pathname}
+                />
+            </Router>,
+            document.getElementById('spa-root')
+        );
+    });
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    renderApp();
+});
