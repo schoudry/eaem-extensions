af480105003db96eae5c1f7243892871d0e71e87
nalabotu <nalabotu@adobe.com>
2021-11-11 08:34:36 -0600

    changes

--- eaem-cloud-service/eaem-spa-page-include/ui.frontend/src/components/Image/Image.js
+++ eaem-cloud-service/eaem-spa-page-include/ui.frontend/src/components/Image/Image.js
@@ -1,7 +1,6 @@
-import { MapTo } from '@adobe/cq-react-editable-components';
-import DOMPurify from 'dompurify';
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import {MapTo} from "@adobe/aem-react-editable-components";
 
 const ImageEditConfig = {
     emptyLabel: 'Image - Experience AEM',
