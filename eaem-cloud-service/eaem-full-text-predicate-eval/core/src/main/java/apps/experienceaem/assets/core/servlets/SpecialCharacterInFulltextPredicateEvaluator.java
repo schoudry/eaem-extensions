d8529da7fc212a06c4735c372e4b87063ee1f2c9
nalabotu <nalabotu@adobe.com>
2021-12-08 05:30:09 -0600

    extend full text predicate

---
+++ eaem-cloud-service/eaem-full-text-predicate-eval/core/src/main/java/apps/experienceaem/assets/core/servlets/SpecialCharacterInFulltextPredicateEvaluator.java
@@ -0,0 +1,27 @@
+package apps.experienceaem.assets.core.servlets;
+
+import com.day.cq.search.Predicate;
+import com.day.cq.search.eval.EvaluationContext;
+import com.day.cq.search.eval.FulltextPredicateEvaluator;
+import org.apache.commons.lang3.StringUtils;
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+        factory = "com.day.cq.search.eval.PredicateEvaluator/fulltext"
+)
+public class SpecialCharacterInFulltextPredicateEvaluator extends FulltextPredicateEvaluator {
+
+    public String getXPathExpression(Predicate predicate, EvaluationContext context) {
+        String searchTerm = predicate.get(predicate.getName());
+
+        if(StringUtils.isEmpty(searchTerm) || !searchTerm.contains("_")){
+            return super.getXPathExpression(predicate, context);
+        }
+
+        searchTerm = searchTerm.replace("_", " ");
+
+        predicate.set("fulltext", searchTerm);
+
+        return super.getXPathExpression(predicate, context);
+    }
+}
