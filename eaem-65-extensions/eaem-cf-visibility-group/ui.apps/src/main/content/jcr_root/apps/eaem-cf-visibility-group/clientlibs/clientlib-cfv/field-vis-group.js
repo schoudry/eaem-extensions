c79b2072aed4f253ff90050ff589ee5b0919b994
nalabotu <nalabotu@adobe.com>
2022-05-16 12:18:30 -0500

    changes

--- eaem-65-extensions/eaem-cf-visibility-group/ui.apps/src/main/content/jcr_root/apps/eaem-cf-visibility-group/clientlibs/clientlib-cfv/field-vis-group.js
+++ eaem-65-extensions/eaem-cf-visibility-group/ui.apps/src/main/content/jcr_root/apps/eaem-cf-visibility-group/clientlibs/clientlib-cfv/field-vis-group.js
@@ -1,14 +1,39 @@
 (function ($, $document) {
-    const url = document.location.pathname;
+    const   URL = document.location.pathname,
+            CFFW = ".coral-Form-fieldwrapper",
+            FIELD_TYPE_SELECTOR = "coral-select[name$='FieldType']";
+    let initialized = false;
 
     if( !isCFEditor() ){
         return;
     }
 
-    alert("hello");
+    init();
+
+    function init(){
+        if(initialized){
+            return;
+        }
+
+        initialized = true;
+
+        window.Dam.CFM.Core.registerReadyHandler(addFieldGrouping);
+    }
+
+    function addFieldGrouping(){
+        $(FIELD_TYPE_SELECTOR).each(function(index, fieldTypeSelect){
+            Coral.commons.ready(fieldTypeSelect, doGrouping);
+        });
+
+        function doGrouping(fieldTypeSelect){
+            _.each(fieldTypeSelect.items.getAll(), (item) => {
+                console.log(item.value);
+            });
+        }
+    }
 
     function isCFEditor(){
-        return ((url.indexOf("/editor.html") == 0)
-            ||  (url.indexOf("/mnt/overlay/dam/cfm/admin/content/v2/fragment-editor.html") == 0) )
+        return ((URL.indexOf("/editor.html") == 0)
+            ||  (URL.indexOf("/mnt/overlay/dam/cfm/admin/content/v2/fragment-editor.html") == 0) )
     }
 }(jQuery, jQuery(document)));
