40d8b3c263839763f304aef2cce2be1d27ea684f
nalabotu <nalabotu@adobe.com>
2022-07-20 13:51:19 -0500

    changes

--- eaem-65-extensions/eaem-sites-csv-export-custom-columns/core/src/main/java/apps/experienceaem/sites/core/filters/AvailableColumnsDSFilter.java
+++ eaem-65-extensions/eaem-sites-csv-export-custom-columns/core/src/main/java/apps/experienceaem/sites/core/filters/AvailableColumnsDSFilter.java
@@ -1,8 +1,16 @@
 package apps.experienceaem.sites.core.filters;
 
 import com.adobe.granite.ui.components.ds.DataSource;
+import com.adobe.granite.ui.components.ds.SimpleDataSource;
+import com.adobe.granite.ui.components.ds.ValueMapResource;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.iterators.TransformIterator;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceMetadata;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
@@ -10,6 +18,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 @Component(
         service = Filter.class,
@@ -24,7 +36,7 @@ import java.io.IOException;
 public class AvailableColumnsDSFilter implements Filter {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public static String DATA_SOURCE_NAME = DataSource.class.getName();
+    public static String EAEM_COLUMN_GROUP = "Experience AEM";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -40,7 +52,37 @@ public class AvailableColumnsDSFilter implements Filter {
 
         DataSource ds = (DataSource)request.getAttribute(DataSource.class.getName());
 
-        logger.info("----" +  ds);
+        final List<ValueMap> dsList = new ArrayList<ValueMap>();
+        Iterator items = ds.iterator();
+
+        while (items.hasNext()) {
+            dsList.add(((ValueMapResource)items.next()).getValueMap());
+        }
+
+        dsList.add(getCustomColumnVM("eaemTitle", "EAEM Title"));
+
+        ds = new SimpleDataSource(new TransformIterator(dsList.iterator(), new Transformer() {
+            public Object transform(Object o) {
+                ValueMap vm = (ValueMap) o;
+
+                return new ValueMapResource(slingRequest.getResourceResolver(), new ResourceMetadata(), "nt:unstructured", vm);
+            }
+        }));
+
+        request.setAttribute(DataSource.class.getName(), ds);
+    }
+
+    private ValueMap getCustomColumnVM(String value, String text){
+        ValueMap vm = new ValueMapDecorator(new HashMap<String, Object>());
+
+        vm.put("value", value);
+        vm.put("default", false);
+        vm.put("text", text);
+        vm.put("show-selector", "");
+        vm.put("columnGroup", EAEM_COLUMN_GROUP);
+        vm.put("description-icon", "");
+
+        return vm;
     }
 
     @Override
