89c976e6c2b5a5768083463ad55cb4347335adc9
nalabotu <nalabotu@adobe.com>
2022-09-22 11:28:41 -0500

    ssr sample

--- eaem-cloud-service/eaem-create-cs-project.bat
+++ eaem-cloud-service/eaem-create-cs-project.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=C:/Progra~1/Java/jdk-11.0.6
 
-mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion=cloud -D appTitle="Experience AEM CS CF Composite Multifield" -D appId="eaem-cf-composite-mf" -D groupId="apps.experienceaem.assets" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=n
+mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM React SSR Sample" -D appId="eaem-sites-react-spa-ssr" -D groupId="apps.experienceaem.sites" -D frontendModule=react -D includeExamples=n -D includeDispatcherConfig=n -DenableSSR=y
 
 REM mvn -B archetype:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion="28" -D appTitle="Experience AEM React SSR" -D aemVersion="cloud" -D appId="eaem-cs-react-ssr" -D groupId="apps.experienceaem.sites" -D enableSSR="y" -D frontendModule="react" -D includeExamples=n -D includeDispatcherConfig=n -U
---
+++ eaem-cloud-service/eaem-sites-react-spa-ssr/ui.frontend/src/index.js
@@ -0,0 +1,78 @@
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9';
+import 'custom-event-polyfill';
+
+import { Constants, ModelManager } from '@adobe/aem-spa-page-model-manager';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { render, hydrate } from 'react-dom';
+import { Router } from 'react-router-dom';
+import App from './App';
+import LocalDevModelClient from './LocalDevModelClient';
+import './components/import-components';
+import './index.css';
+
+const modelManagerOptions = {};
+if(process.env.REACT_APP_PROXY_ENABLED) {
+    modelManagerOptions.modelClient = new LocalDevModelClient(process.env.REACT_APP_API_HOST);
+}
+
+const renderApp = () => {
+    ModelManager.initialize(modelManagerOptions).then(pageModel => {
+        const history = createBrowserHistory();
+        render(
+            <Router history={history}>
+                <App
+                    history={history}
+                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
+                    cqItems={pageModel[Constants.ITEMS_PROP]}
+                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
+                    cqPath={pageModel[Constants.PATH_PROP]}
+                    locationPathname={window.location.pathname}
+                />
+            </Router>,
+            document.getElementById('spa-root')
+        );
+    });
+};
+
+const hydrateApp = (initialState) => {
+
+    modelManagerOptions.model = initialState.rootModel;
+    ModelManager.initialize(modelManagerOptions).then(pageModel => {
+        const history = createBrowserHistory();
+        hydrate(
+            <Router history={history}>
+                <App
+                    history={history}
+                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
+                    cqItems={pageModel[Constants.ITEMS_PROP]}
+                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
+                    cqPath={pageModel[Constants.PATH_PROP]}
+                    locationPathname={window.location.pathname}
+                />
+            </Router>,
+            document.getElementById('spa-root')
+        );
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const initialStateScriptTag = document.getElementById('__INITIAL_STATE__');
+    if(!!initialStateScriptTag){
+        try{
+            const initialState = JSON.parse(initialStateScriptTag.innerHTML);
+            initialStateScriptTag.remove();
+            hydrateApp(initialState);
+
+            console.info('hydrated react DOM');
+        }catch(err){
+            console.error('failed to parse initial state json! re-rendering output.', err);
+            renderApp();
+        }
+    }else{
+        renderApp();
+    }
+});
