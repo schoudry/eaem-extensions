bdc8dd39da4f614a17e5befa90ec4e484fba6cf6
nalabotu <nalabotu@adobe.com>
2022-11-07 16:39:29 -0600

    svg stream

--- eaem-cloud-service/eaem-create-cs-project.bat
+++ eaem-cloud-service/eaem-create-cs-project.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=C:/Progra~1/Java/jdk-11.0.6
 
-mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM Smart Crop Jsonp Cache" -D appId="eaem-dm-smart-crop-jsonp-cache" -D groupId="apps.eaem.assets" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=n
+mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM SVG Stream Clear Cache" -D appId="eaem-svg-stream-clear-cache" -D groupId="apps.eaem.assets" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=y
 
 REM mvn -B archetype:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion="28" -D appTitle="Experience AEM React SSR" -D aemVersion="cloud" -D appId="eaem-cs-react-ssr" -D groupId="apps.experienceaem.sites" -D enableSSR="y" -D frontendModule="react" -D includeExamples=n -D includeDispatcherConfig=n -U
---
+++ eaem-cloud-service/eaem-svg-stream-clear-cache/core/src/main/java/apps/eaem/assets/core/servlets/SVGExporter.java
@@ -0,0 +1,131 @@
+package apps.eaem.assets.core.servlets;
+
+import com.day.cq.dam.api.Asset;
+import com.day.cq.wcm.api.Page;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+
+@Component(
+        name = "Experience AEM SVG Exporter",
+        immediate = true,
+        service = Servlet.class,
+        property = {
+                "sling.servlet.methods=GET",
+                "sling.servlet.paths=/bin/eaem/svgs"
+        }
+)
+public class SVGExporter extends SlingSafeMethodsServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SVGExporter.class);
+
+    private static final String ICONS_FOLDER = "/content/dam/eaem-svg-stream-clear-cache";
+    private static final String ID_ATTRIBUTE = "id";
+    private static final String XLINK_ATTRIBUTE = "xlink:href";
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+                        throws ServletException, IOException {
+        List<Resource> icons = new ArrayList<Resource>();
+
+        Resource iconsFolder = request.getResourceResolver().getResource(ICONS_FOLDER);
+
+        List<String> iconStrings = getAllResourceChildren(iconsFolder, r -> r.getName().endsWith(".svg"), icons)
+                .stream()
+                .map(this::getIconAsString)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        response.setContentType("image/svg+xml; charset=UTF-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.write("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+        iconStrings.forEach(writer::write);
+        writer.write("</svg>");
+    }
+
+    private static Collection<Resource> getAllResourceChildren(Resource resource, Predicate<Resource> predicate,
+                                                               List<Resource> collection) {
+        Iterable<Resource> children = resource.getChildren();
+
+        for (Resource child : children) {
+            if(predicate.test(child)){
+                collection.add(child);
+            }else{
+                getAllResourceChildren(child, predicate, collection);
+            }
+        }
+
+        return collection;
+    }
+
+    private String getIconAsString(Resource resource) {
+        String result = null;
+        String name = FilenameUtils.getBaseName(resource.getName());
+        Asset asset = resource.adaptTo(Asset.class);
+
+        if (asset == null) {
+            return result;
+        }
+
+        try{
+            String content = IOUtils.toString(asset.getOriginal().getStream());
+            result = transformSvg(name, content);
+        } catch (IOException e) {
+            LOGGER.error("Error reading svg stream: {}", e);
+        }
+
+        return result;
+    }
+
+    public static String transformSvg(String id, String svgContent) {
+        Document doc = Jsoup.parse(svgContent, "", Parser.xmlParser());
+        Element svg = doc.select("svg").first();
+
+        Element symbol = doc.createElement("symbol");
+        symbol.attr(ID_ATTRIBUTE, id);
+        symbol.attr("viewBox", svg.attr("viewBox"));
+
+        Optional.ofNullable(svg.select("style").first()).ifPresent(style -> {
+            String cssSelector = String.format("#%s .$1{", id);
+            String styleContent = style.html().replaceAll("\\.(-?[_a-zA-Z]+[_a-zA-Z0-9-]*\\s*)\\{", cssSelector);
+            style.html(styleContent);
+        });
+
+        svg.children().forEach(child -> {
+            child.getElementsByAttribute(ID_ATTRIBUTE).forEach(element -> {
+                String value = String.format("%s-%s", id, element.attr(ID_ATTRIBUTE));
+                element.attr(ID_ATTRIBUTE, value);
+            });
+            child.getElementsByAttribute(XLINK_ATTRIBUTE).forEach(element -> {
+                String value = String.format("#%s-%s", id, element.attr(XLINK_ATTRIBUTE).substring(1));
+                element.attr(XLINK_ATTRIBUTE, value);
+            });
+
+            symbol.appendChild(child);
+        });
+
+        return symbol.outerHtml();
+    }
+}
