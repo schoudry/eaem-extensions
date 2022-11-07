6a1f3ea17a9a176f82362468bb53ec8ea9cd829e
nalabotu <nalabotu@adobe.com>
2022-11-07 17:24:46 -0600

    changes

---
+++ eaem-cloud-service/eaem-svg-stream-clear-cache/ui.apps/src/main/content/jcr_root/apps/eaem-svg-stream-clear-cache/clientlibs/clientlib-base/load-svgs.js
@@ -0,0 +1,10 @@
+(function(){
+    const SVG_URL = "/bin/eaem/svgs";
+
+    function createSpriteDiv(){
+        var elemDiv = document.createElement('div');
+        document.body.appendChild(elemDiv);
+    }
+
+    createSpriteDiv();
+}())
