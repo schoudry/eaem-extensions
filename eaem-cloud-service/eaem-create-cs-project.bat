ff77f3232c3a2647516fec83b3be48b255ac5d3e
nalabotu <nalabotu@adobe.com>
2023-03-29 16:21:15 -0500

    filter nav items

--- eaem-cloud-service/eaem-create-cs-project.bat
+++ eaem-cloud-service/eaem-create-cs-project.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=C:/Progra~1/Java/jdk-11.0.6
 
-mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM Custom Env Banners" -D appId="eaem-env-var-banner" -D groupId="apps.eaem.platform" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=n
+mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM Filter Nav Items" -D appId="eaem-filter-nav-items" -D groupId="apps.experienceaem.platform" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=n
 
 REM mvn -B archetype:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion="28" -D appTitle="Experience AEM React SSR" -D aemVersion="cloud" -D appId="eaem-cs-react-ssr" -D groupId="apps.experienceaem.sites" -D enableSSR="y" -D frontendModule="react" -D includeExamples=n -D includeDispatcherConfig=n -U
---
+++ eaem-cloud-service/eaem-filter-nav-items/core/src/main/java/apps/experienceaem/platform/core/servlets/NavItemsDataSourceFilter.java
@@ -0,0 +1,116 @@
+package apps.experienceaem.platform.core.servlets;
+
+import com.adobe.granite.ui.components.ds.DataSource;
+import com.adobe.granite.ui.components.ds.SimpleDataSource;
+import org.apache.commons.collections4.iterators.TransformIterator;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceWrapper;
+import com.adobe.granite.ui.components.Config;
+import org.apache.sling.api.resource.ValueMap;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.collections4.Transformer;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+@Component(
+    service = Filter.class,
+    immediate = true,
+    property = {
+        Constants.SERVICE_RANKING + ":Integer=-99",
+        "sling.filter.scope=COMPONENT",
+        "sling.filter.resourceTypes=granite/ui/components/shell/globalnav/datasources/navitems"
+    }
+)
+public class NavItemsDataSourceFilter implements Filter {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final String NAV_ASSETS_PATH = "/mnt/overlay/cq/core/content/nav/assets";
+
+    private static final List EXCLUDE_ITEMS = Arrays.asList(new String[]{"/mnt/overlay/cq/core/content/nav/assets/asyncjobs"});
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+        SlingHttpServletResponse slingResponse = (SlingHttpServletResponse) response;
+
+        try {
+            if (!slingRequest.getPathInfo().endsWith(NAV_ASSETS_PATH)) {
+                chain.doFilter(slingRequest, slingResponse);
+                return;
+            }
+
+            Resource slingRequestResource = slingRequest.getResource();
+            Resource repConfigRes = slingRequestResource.getChild(Config.DATASOURCE);
+
+            if (repConfigRes == null) {
+                chain.doFilter(slingRequest, slingResponse);
+                return;
+            }
+
+            chain.doFilter(slingRequest, slingResponse);
+
+            ValueMap repConfigVM = repConfigRes.getValueMap();
+            final String itemRT = repConfigVM.get("itemResourceType", "");
+
+            SimpleDataSource ds = (SimpleDataSource) request.getAttribute(DataSource.class.getName());
+
+            if (ds == null) {
+                chain.doFilter(slingRequest, slingResponse);
+                return;
+            }
+
+            final List<Resource> dsList = new ArrayList<Resource>();
+            Iterator<Resource> items = ds.iterator();
+            Resource resource = null;
+
+            while (items.hasNext()) {
+                resource = (Resource) items.next();
+
+                if (EXCLUDE_ITEMS.contains(resource.getPath())) {
+                    continue;
+                }
+
+                dsList.add(resource);
+            }
+
+            ds = new SimpleDataSource(new TransformIterator(dsList.iterator(), new Transformer<Resource, Resource>() {
+                public Resource transform(Resource r) {
+                    return new ResourceWrapper(r) {
+                        public String getResourceType() {
+                            return r.getValueMap().get("itemResourceType", itemRT);
+                        }
+
+                        public Iterable<Resource> getChildren() {
+                            return new ArrayList<Resource>();
+                        }
+                    };
+                }
+            }));
+
+            request.setAttribute(DataSource.class.getName(), ds);
+        } catch (Exception e) {
+            logger.error("Error working with nav items list", e);
+        }
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
