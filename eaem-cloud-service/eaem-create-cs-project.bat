f81538a2b1cfa3dd273e095a5f3f9987f77bc55f
nalabotu <nalabotu@adobe.com>
2023-04-10 12:58:04 -0500

    error directives

--- eaem-cloud-service/eaem-create-cs-project.bat
+++ eaem-cloud-service/eaem-create-cs-project.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=C:/Progra~1/Java/jdk-11.0.6
 
-mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM Core Image Component Sling Delegate" -D appId="eaem-image-comp-sling-delegate" -D groupId="apps.experienceaem.sites" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=n
+mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion=36 -D aemVersion="cloud" -D appTitle="Experience AEM Dispatcher Error Directives" -D appId="eaem-dispatcher-error-directives" -D groupId="apps.experienceaem.sites" -D frontendModule=none -D includeExamples=n -D includeDispatcherConfig=y
 
 REM mvn -B archetype:generate -D archetypeGroupId=com.adobe.aem -D archetypeArtifactId=aem-project-archetype -D archetypeVersion="28" -D appTitle="Experience AEM React SSR" -D aemVersion="cloud" -D appId="eaem-cs-react-ssr" -D groupId="apps.experienceaem.sites" -D enableSSR="y" -D frontendModule="react" -D includeExamples=n -D includeDispatcherConfig=n -U
