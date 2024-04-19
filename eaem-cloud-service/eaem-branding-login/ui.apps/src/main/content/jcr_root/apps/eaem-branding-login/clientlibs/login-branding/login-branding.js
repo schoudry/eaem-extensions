93edc8a8460df771af09791e6944526ef1c02e57
nalabotu <nalabotu@adobe.com>
2024-04-19 13:17:45 -0500

    changes

--- eaem-cloud-service/eaem-branding-login/ui.apps/src/main/content/jcr_root/apps/eaem-branding-login/clientlibs/login-branding/login-branding.js
+++ eaem-cloud-service/eaem-branding-login/ui.apps/src/main/content/jcr_root/apps/eaem-branding-login/clientlibs/login-branding/login-branding.js
@@ -1,7 +1,31 @@
 (function(document) {
+    const LOGIN_PAGE = "/libs/granite/core/content/login.html",
+            BG_DEFAULT_SEL = "#bg_default",
+            LOGGIN_BOX_SEL = "#login-box";
+
     document.addEventListener("DOMContentLoaded", changeHelpText);
 
     function changeHelpText(event){
-        alert("hello");
+        if(!isLoginPage()){
+            return;
+        }
+
+        let loginBoxH1 = document.querySelector(LOGGIN_BOX_SEL + " h1");
+
+        loginBoxH1.innerText = "Experiencing Adobe Experience Manager";
+        loginBoxH1.style.backgroundColor = "#cc4400";
+        loginBoxH1.style.padding = "5px";
+
+        let loginBoxP = document.querySelector(LOGGIN_BOX_SEL + " p");
+        loginBoxP.innerText = "Aodbe IMS users please use 'Sign in with Adobe'";
+        loginBoxP.style.backgroundColor = "#cc4400";
+        loginBoxP.style.padding = "5px";
+
+        let loginBG = document.querySelector(BG_DEFAULT_SEL);
+        loginBG.style.backgroundImage = "url(https://wallpapercave.com/wp/wp10747952.jpg)";
     }
-});
+
+    function isLoginPage(){
+        return (window.location.pathname === LOGIN_PAGE);
+    }
+}(document));
