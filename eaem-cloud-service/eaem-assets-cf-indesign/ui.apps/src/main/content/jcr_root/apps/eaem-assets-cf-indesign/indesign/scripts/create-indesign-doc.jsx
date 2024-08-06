2061c566cca14096a52585e6a714001ea7dce1a3
nalabotu <sreekanth.choudry@gmail.com>
2024-08-06 17:28:29 -0500

    Update create-indesign-doc.jsx

--- eaem-cloud-service/eaem-assets-cf-indesign/ui.apps/src/main/content/jcr_root/apps/eaem-assets-cf-indesign/indesign/scripts/create-indesign-doc.jsx
+++ eaem-cloud-service/eaem-assets-cf-indesign/ui.apps/src/main/content/jcr_root/apps/eaem-assets-cf-indesign/indesign/scripts/create-indesign-doc.jsx
@@ -2,16 +2,11 @@
     var returnObj = {}, aemHost, base64EncodedAEMCreds, resourcePath, document,
         aemUploadPath, contentJson;
 
-    function createPDF() {
+    function createInDesignDoc() {
         var sourceFolder = getSourceFolder(),
             fileName = resourcePath.substring(resourcePath.lastIndexOf ('/') + 1),
             xmlFile = new File( sourceFolder.fullName + "/" + fileName + ".xml"),
             sourceFile = new File(sourceFolder.fullName + "/" + fileName);
-
-        with (app.pdfExportPreferences) {
-            viewDocumentAfterExport = false;
-            pageRange = PageRange.ALL_PAGES;
-        }
 
         app.consoleout('Fetching resource from AEM: ' + aemHost + resourcePath + ' to ' + sourceFile);
 
@@ -21,71 +16,7 @@
 
         app.consoleout('Started PDF export - ' + pdfOutputFile);
 
-        if(contentJson){
-            document = app.open(sourceFile);
-
-            document.exportFile(ExportFormat.xml, xmlFile);
-
-            xmlFile.open('r');
-
-            var xmlContent = xmlFile.read();
-
-            xmlFile.close();
-
-            var structureXML = new XML (xmlContent),
-                eles = structureXML.elements(), element, eleName, data;
-
-            for(var x = 0 ; x < eles.length(); x++){
-                element = eles.child(x);
-                eleName = element.name();
-                data = contentJson[eleName];
-
-                if(data){
-                    if(data.type === "JCR_PATH"){
-                        var jcrName = data.value.substring(data.value.lastIndexOf ('/') + 1);
-
-                        if(jcrName.indexOf("cq5dam.") == 0){
-                            jcrName = data.value.substring(0, data.value.lastIndexOf ('/jcr:content/renditions'));
-                            jcrName = jcrName.substring(jcrName.lastIndexOf ('/') + 1);
-                        }
-
-                        var jcrFile = new File(sourceFolder.fullName + "/" + jcrName);
-
-                        app.consoleout('Fetching resource : ' + data.value + ' to ' + jcrFile);
-
-                        fetchResource (aemHost,  base64EncodedAEMCreds, data.value, jcrFile);
-
-                        structureXML[eleName].@href = "file://./" + jcrName;
-                    }else{
-                        structureXML[eleName] = data.value;
-                    }
-                }else{
-                    delete structureXML[eleName];
-                }
-            }
-
-            xmlFile.encoding = "UTF8";
-
-            xmlFile.open("w");
-
-            xmlContent = structureXML.toString();
-
-            xmlFile.write(xmlContent);
-
-            xmlFile.close();
-
-            document.importXML(xmlFile);
-        }
-
-        document.exportFile(ExportFormat.pdfType, pdfOutputFile);
-
-        app.consoleout('Uploading PDF to - ' + aemUploadPath);
-
-        putResource(aemHost, base64EncodedAEMCreds, pdfOutputFile, pdfOutputFile.name, 'application/pdf', aemUploadPath);
-
-        sourceFolder.remove();
-
-        app.consoleout('PDF export complete...');
+        app.consoleout('contentJson - ' + contentJson);
 
         returnObj.success = "completed";
     }
@@ -114,12 +45,6 @@
             throw "aemHost argument is missing";
         }
 
-        if (app.scriptArgs.isDefined("resourcePath")) {
-            resourcePath = app.scriptArgs.getValue("resourcePath");
-        } else {
-            throw "resourcePath argument missing";
-        }
-
         if (app.scriptArgs.isDefined("contentJson")) {
             contentJson = JSON.parse(app.scriptArgs.getValue("contentJson"));
             app.consoleout('contentJson: ' + app.scriptArgs.getValue("contentJson"));
@@ -127,12 +52,8 @@
             throw "contentJson argument missing";
         }
 
-        aemUploadPath = app.scriptArgs.getValue("aemUploadPath");
-
         app.consoleout('base64EncodedAEMCreds --- ' + base64EncodedAEMCreds);
         app.consoleout('aemHost --- ' + aemHost);
-        app.consoleout('resourcePath --- ' + resourcePath);
-        app.consoleout('aemUploadPath --- ' + aemUploadPath);
     }
 
     try{
@@ -140,7 +61,7 @@
 
         //setTestParams();
 
-        createPDF();
+        createInDesignDoc();
     }catch(err){
         returnObj.error = err;
         app.consoleout("Error processing document : " + resourcePath + ", error : " + err);
