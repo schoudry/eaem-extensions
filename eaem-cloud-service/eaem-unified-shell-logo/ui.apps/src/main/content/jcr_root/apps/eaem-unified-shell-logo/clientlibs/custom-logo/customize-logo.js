92746a6653b28060baa4cdb335d4e9bc95843935
nalabotu <sreekanth.choudry@gmail.com>
2024-08-29 17:56:51 -0500

    changes

--- eaem-cloud-service/eaem-unified-shell-logo/ui.apps/src/main/content/jcr_root/apps/eaem-unified-shell-logo/clientlibs/custom-logo/customize-logo.js
+++ eaem-cloud-service/eaem-unified-shell-logo/ui.apps/src/main/content/jcr_root/apps/eaem-unified-shell-logo/clientlibs/custom-logo/customize-logo.js
@@ -3,24 +3,37 @@
         return;
     }
 
+    const US_CONFIG_URL = "/bin/eaem/usconfig";
+
     let usHeader = window.parent.document.querySelector("header");
 
-    usHeader.style.display = "none"
+    usHeader.style.display = "none";
 
-    let heroTitle = usHeader.getElementsByClassName("hero-title");
+    $.ajax(US_CONFIG_URL).done( json => {
+        let heroTitle = usHeader.getElementsByClassName("hero-title");
 
-    if(heroTitle && heroTitle.length > 0){
-        heroTitle[0].innerHTML = "Experience AEM";
-    }
+        if(json["title"] && heroTitle && heroTitle.length > 0){
+            heroTitle[0].innerHTML = json["title"];
+        }
 
-    let heroImage = usHeader.getElementsByClassName("hero-svg");
+        let heroImage = usHeader.getElementsByClassName("hero-svg");
 
-    if(heroImage && heroImage.length > 0){
-        let logoPath = "/apps/eaem-unified-shell-logo/clientlibs/custom-logo/logo.png";
-        heroImage[0].src = logoPath;
-    }
+        if(json["logo"] && heroImage && heroImage.length > 0){
+            heroImage[0].src = json["logo"];
+        }
 
-    setTimeout(() => {
-        usHeader.style.display = "block";
-    }, 500)
+        if(json["env"]){
+            const INTERVAL = setInterval(function(){
+                let envLabel = usHeader.getElementsByClassName("colorBadge");
+
+                if(envLabel && envLabel.length > 0){
+                    clearInterval(INTERVAL);
+                    envLabel[0].innerHTML = json["env"];
+                    usHeader.style.display = "block";
+                }
+            }, 250);
+        }else{
+            usHeader.style.display = "block";
+        }
+    });
 }());
