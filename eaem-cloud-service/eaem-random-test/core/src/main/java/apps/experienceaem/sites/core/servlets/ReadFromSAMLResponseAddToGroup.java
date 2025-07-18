d4399a8e1563760c6de6f06e32acf96ad35dce59
nalabotu <sreekanth.choudry@gmail.com>
2025-07-18 16:58:54 -0500

    changes

--- eaem-cloud-service/eaem-random-test/core/src/main/java/apps/experienceaem/sites/core/servlets/ReadFromSAMLResponseAddToGroup.java
+++ eaem-cloud-service/eaem-random-test/core/src/main/java/apps/experienceaem/sites/core/servlets/ReadFromSAMLResponseAddToGroup.java
@@ -1,14 +1,24 @@
 package apps.experienceaem.sites.core.servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.apache.commons.io.IOUtils;
-import org.apache.jackrabbit.api.JackrabbitSession;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -17,6 +27,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.jcr.Session;
+import javax.jcr.security.*;
 import javax.servlet.Servlet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -26,9 +37,8 @@ import javax.xml.xpath.XPathFactory;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import org.apache.jackrabbit.commons.jackrabbit.authorization.AccessControlUtils;
 
 @Component(
         immediate = true,
@@ -41,9 +51,32 @@ import java.util.Set;
 public class ReadFromSAMLResponseAddToGroup extends SlingSafeMethodsServlet {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+
+    String restrictedSAMLGroup = "github-copilot-users";
+
+    JsonObject restrictedPreviewGroupsJson = new JsonObject();
+
+    @Activate
+    protected void activate(final ReadFromSAMLResponseAddToGroup.ReadFromSAMLResponseAddToGroupConfig config) {
+        logger.info("[activate] - Activating ReadFromSAMLResponseAddToGroupConfig");
+
+        try{
+            restrictedPreviewGroupsJson = parseJsonString(config.getRestrictedPreviewGroupsJson());
+        }catch (Exception e){
+            logger.error("Error reading configuration", e);
+        }
+    }
+
     @Override
     protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp) {
         try{
+            ResourceResolver serviceResolver= getUserManagementServiceResolver();
+            UserManager um = serviceResolver.adaptTo(UserManager.class);
+
+            Authorizable userAuthorizable = um.getAuthorizable(req.getUserPrincipal().getName());
+
             String samlResponseBase64 = "";
 
             InputStream inputStream = new ByteArrayInputStream(Base64.getDecoder().decode(samlResponseBase64));
@@ -61,6 +94,8 @@ public class ReadFromSAMLResponseAddToGroup extends SlingSafeMethodsServlet {
 
             NodeList itemList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
 
+            boolean groupExistsInIDP = true;
+
             for (int i = 0; i < itemList.getLength(); i++) {
                 Node item = itemList.item(i);
 
@@ -68,8 +103,18 @@ public class ReadFromSAMLResponseAddToGroup extends SlingSafeMethodsServlet {
                     Element eElement = (Element) item;
                     String samlGroup = eElement.getTextContent();
 
-                    resp.getWriter().write(samlGroup + "\r\n");
+                    if(samlGroup.trim().startsWith("CN=" + restrictedSAMLGroup)){
+                        addUserToGroup(serviceResolver, restrictedSAMLGroup, userAuthorizable);
+                    }
                 }
+            }
+
+            if(!groupExistsInIDP){
+                removeUserFromGroup(restrictedSAMLGroup, userAuthorizable);
+            }
+
+            if (serviceResolver.hasChanges()) {
+                serviceResolver.commit();
             }
 
         } catch (Exception e) {
@@ -77,14 +122,83 @@ public class ReadFromSAMLResponseAddToGroup extends SlingSafeMethodsServlet {
         }
     }
 
-    private void addUserToGroup(Session serviceSession, String groupName, String userName) throws Exception{
-        UserManager um = ((JackrabbitSession) serviceSession).getUserManager();
+    private void setPermissionsOnPaths(String[] paths, Session serviceSession, Group group) throws Exception{
+        boolean isAllow = true;
+
+        for(String path : paths){
+            if(serviceSession.itemExists(path)){
+                logger.info("Setting allow permission for group {}, on path {}", group.getID(), path);
+                AccessControlUtils.addAccessControlEntry(serviceSession, path, group.getPrincipal(), new String[]{ Privilege.JCR_READ }, isAllow);
+            }else{
+                logger.info("Cannot set allow permission for group {}, on path {} as the path does not exist", group.getID(), path)
+            }
+        }
+    }
+
+    private void removeUserFromGroup(String groupName, Authorizable userAuthorizable) throws Exception{
+        for (Iterator<Group> memOfGroups = userAuthorizable.declaredMemberOf(); memOfGroups.hasNext();) {
+            Group mGroup = memOfGroups.next();
+
+            if(mGroup.getID().equals(groupName)){
+                mGroup.removeMember(userAuthorizable);
+                logger.info("Removed user {} from group {}", userAuthorizable.getPath(), groupName);
+            }
+        }
+    }
+
+    private Group addUserToGroup(ResourceResolver serviceResolver, String groupName, Authorizable userAuthorizable) throws Exception{
+        UserManager um = serviceResolver.adaptTo(UserManager.class);
 
         Authorizable authorizable = um.getAuthorizable(groupName);
+        Group group = null;
 
-        if (authorizable != null && authorizable.isGroup()) {
-            Group group = (Group) authorizable;
-            group.addMember(um.getAuthorizable(userName));
+        if (authorizable == null || !authorizable.isGroup()) {
+            logger.info("No AEM group found {}, creating it", groupName);
+            group = um.createGroup(groupName);
+        }else{
+            group = (Group) authorizable;
         }
+
+        if(restrictedPreviewGroupsJson.has(groupName)){
+            Session serviceSession = serviceResolver.adaptTo(Session.class);
+            JsonElement rootPathsForGroup = restrictedPreviewGroupsJson.get(groupName);
+            String[] paths = null;
+
+            if(rootPathsForGroup.isJsonArray()){
+                paths = new Gson().fromJson(rootPathsForGroup, String[].class);
+            }else{
+                paths = new String[] { rootPathsForGroup.getAsString() };
+            }
+
+            setPermissionsOnPaths(paths, serviceSession, group);
+        }
+
+        group.addMember(userAuthorizable);
+
+        logger.info("Added user {} to group {}", userAuthorizable.getPath(), groupName);
+
+        return group;
+    }
+
+    private ResourceResolver getUserManagementServiceResolver() throws Exception{
+        Map<String, Object> USER_SERVICE_MAP = new HashMap<>();
+        USER_SERVICE_MAP.put(ResourceResolverFactory.SUBSERVICE, "eaem-user-service");
+
+        return resourceResolverFactory.getServiceResourceResolver(USER_SERVICE_MAP);
+    }
+
+    public static JsonObject parseJsonString(String jsonString) {
+        return StringUtils.isNotBlank(jsonString) && new Gson().fromJson(jsonString, JsonObject.class).isJsonObject()
+                ? new Gson().fromJson(jsonString, JsonObject.class).getAsJsonObject()
+                : new JsonObject();
+    }
+
+    @ObjectClassDefinition(name = "ReadFromSAMLResponseAddToGroup Configuration",
+            description = "ReadFromSAMLResponseAddToGroup Configuration")
+    public @interface ReadFromSAMLResponseAddToGroupConfig {
+        @AttributeDefinition(name = "Search Environment Variable",
+                description = "Provide the Vault Private Key to fetch variable from environment configuration",
+                type = AttributeType.STRING)
+        String getRestrictedPreviewGroupsJson();
     }
 }
