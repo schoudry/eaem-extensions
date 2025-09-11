5d7e1e8b7d535524bb8c4c9f5c4f240d7e0b9981
nalabotu <sreekanth.choudry@gmail.com>
2025-09-11 16:25:32 -0500

    update-props

---
+++ eaem-misc/eaem-update-props-node-js/update-props.js
@@ -0,0 +1,143 @@
+const https = require('https'); 
+const fs = require("fs"); 
+ 
+const AEM_HOST = 'author-p10961-e880305.adobeaemcloud.com'; 
+const AEM_TOKEN = "eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE3NTg1NzUzMDc1MjVfYmM3NjUzNjQtMDExMS00NWRiLTg5NzQtMjgzMTA3MTJmNDQ5X3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJkZXYtY29uc29sZS1wcm9kIiwidXNlcl9pZCI6IjgwOUQxRjU0NjMxQzEzNEMwQTQ5NUUyRUA4MjllMWViYjYzMWMwNGNmNDk1YzYwLmUiLCJzdGF0ZSI6IkdXak5ZVUVmWDEyOWJ4eDFhUk5HVjR4MCIsImFzIjoiaW1zLW5hMSIsImFhX2lkIjoiMDc4QzNFOTA1MjRDOUE1RTBBNDkwRDQ1QEFkb2JlSUQiLCJjdHAiOjAsImZnIjoiWlo0Q1NTTlVWTE01QURVS0hBUVZJWEFBSlkiLCJzaWQiOiIxNzU4MTI2MDQwODgwXzMxYjdlOGIyLWZmODEtNDY4Yy1iM2NmLWQxZjFlOTQyOWE3ZV91dzIiLCJydGlkIjoiMTc1ODU3NTMwNzUyNV9kNWVmOGZmMy1jYjllLTRiNGYtYjEyYy0yNzQyMjYyYmNmYzJfdWUxIiwibW9pIjoiNWVjN2JlMjIiLCJwYmEiOiJNZWRTZWNOb0VWLExvd1NlYyIsInJ0ZWEiOiIxNzU5Nzg0OTA3NTI1IiwiZXhwaXJlc19pbiI6Ijg2NDAwMDAwIiwic2NvcGUiOiJBZG9iZUlELG9wZW5pZCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0LGFkZGl0aW9uYWxfaW5mby5yb2xlcyIsImNyZWF0ZWRfYXQiOiIxNzU4NTc1MzA3NTI1In0.iYVVEGnY32LwqxgYZnjGcefiP8ABTp9mN9z1T0T6mvnJk9fSiYLPChWZVDEOjjuRbTdaRPKZYL4WnSqEtf9Id6GPecYIuAqDu9_dqCnAqna9y_Qc00wg9IsR0k0eiJtEOlFNm6Vka9JFE1wkhZEahTzSY6QFQcDFe5ur3nhi3wCS9G6mNrKT8-7jBmoUeh5aO_9VC67QcaslJlnAj71LP3U4fhpEPJJ96c_AZuH2c9CvQEYe7YhbL3WXEhjc76dw9_pI2c8izAXD5TWV_zHnZ8LtQkINBOvYWpt4QVaIirs41OH3atVO6AepQfEpyn3YhnjYISHLJgtxTRmBU1Nhiw"; 
+const FILE_PREFIX = "p10961-e880305"; 
+const LOG = "./" + FILE_PREFIX + ".log"; 
+const RESULTS_FILE_PATH = "./" + FILE_PREFIX + ".txt"; 
+const FAILED_PATHS = "./" + FILE_PREFIX + "-failed.txt"; 
+const SUCCESS_PATHS = "./" + FILE_PREFIX + "-success.txt"; 
+const QUERY = "/bin/querybuilder.json?path=/content/eaem-random-test&type=cq:Page&p.hits=selective&p.properties=jcr:path&p.limit=-1"
+const PROP_1 = "cq:contextHubPath"; 
+const PROP_1_VALUE = "/conf/global/settings/cloudsettings/default/contexthub"; 
+const PROP_2 = "cq:contextHubSegmentsPath"; 
+const PROP_2_VALUE = "/conf/global/settings/wcm/segments"; 
+ 
+startProcess(); 
+ 
+async function startProcess(){ 
+    try { 
+        let results = await doFetchSyncGet(AEM_HOST, AEM_TOKEN, QUERY);
+
+        let hits = JSON.parse(results).hits;
+
+        for (const hit of hits) {
+            writeResultsToFile(hit["jcr:path"]);
+        }
+
+        const fileContent = fs.readFileSync(RESULTS_FILE_PATH, 'utf-8'); 
+        const lines = fileContent.split('\n'); 
+ 
+        for (const line of lines) { 
+            await updateProperty(AEM_HOST, AEM_TOKEN, line.trim()); 
+        } 
+    } catch (e) { 
+        console.error(e); 
+    } 
+} 
+
+function writeResultsToFile(path){
+    fs.appendFileSync(RESULTS_FILE_PATH, path + "\n");
+}
+ 
+async function updateProperty(aemHost, aemToken, pagePath) { 
+    if(!pagePath){ 
+        return; 
+    } 
+ 
+    const postData = { 
+        [PROP_1]: PROP_1_VALUE, 
+        [PROP_2]: PROP_2_VALUE 
+    }; 
+ 
+    await doFetchSyncPost(aemHost, aemToken, pagePath, postData); 
+} 
+ 
+async function doFetchSyncPost(aemHost, token, path, payload) { 
+    let postOptions = { 
+        method: "POST", 
+        headers: { 
+            "Content-Type": "application/x-www-form-urlencoded" 
+        }, 
+        body: new URLSearchParams(payload), 
+    } 
+ 
+    if (aemHost == "localhost") { 
+        aemHost = "http://" + aemHost + ":4502"; 
+        postOptions.headers["cookie"] = "login-token=" + token; 
+    } else { 
+        aemHost = "https://" + aemHost; 
+        postOptions.headers["Authorization"] = "Bearer " + token; 
+    } 
+ 
+    let data; 
+ 
+    try { 
+        const response = await fetch(aemHost + path + "/jcr:content", postOptions); 
+ 
+        data = await response.text(); 
+ 
+        writeToSuccess(path); 
+ 
+        logMessage(path + ", Updated : " + PROP_1 + "," + PROP_2); 
+    } catch (err) { 
+        logMessage("ERROR: " + path + " : " + err.message); 
+        console.log(err); 
+ 
+        writeToFailed(path); 
+ 
+        console.log(" ##### BREATHING FOR A SECOND  #################### "); 
+        await delay(10000); 
+    } 
+ 
+    return data; 
+} 
+
+async function doFetchSyncGet(aemHost, token, path){
+    let getOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+
+    if(aemHost == "localhost"){
+        aemHost = "http://" + aemHost + ":4502";
+        getOptions.headers["cookie"] = "login-token=" + token;
+    }else{
+        aemHost = "https://" + aemHost;
+        getOptions.headers["Authorization"] = "Bearer " + token;
+    }
+
+    let data = {};
+
+    try{
+        const response = await fetch(aemHost + path, getOptions);
+        data = await response.text();
+
+        return data;
+    }catch(err){
+        console.log(err); 
+    }
+
+    return data;
+}
+ 
+function delay(time) { 
+  return new Promise(resolve => setTimeout(resolve, time)); 
+} 
+ 
+function writeToFailed(path){ 
+    fs.appendFileSync(FAILED_PATHS, path + "\n"); 
+} 
+ 
+function writeToSuccess(path){ 
+    fs.appendFileSync(SUCCESS_PATHS, path + "\n"); 
+} 
+ 
+function logMessage(message){ 
+    console.log(message); 
+    fs.appendFileSync(LOG, message + "\n"); 
+} 
+ 
