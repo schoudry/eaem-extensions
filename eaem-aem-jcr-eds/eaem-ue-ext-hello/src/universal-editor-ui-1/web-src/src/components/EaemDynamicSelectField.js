6b3777fddc3a226bc09c3cf0db8811f069f2655f
nalabotu <sreekanth.choudry@gmail.com>
2025-12-24 10:20:35 -0500

    Update EaemDynamicSelectField.js

--- eaem-aem-jcr-eds/eaem-ue-ext-hello/src/universal-editor-ui-1/web-src/src/components/EaemDynamicSelectField.js
+++ eaem-aem-jcr-eds/eaem-ue-ext-hello/src/universal-editor-ui-1/web-src/src/components/EaemDynamicSelectField.js
@@ -12,8 +12,38 @@ import { extensionId } from "./Constants"
 
 export default function EaemDynamicSelectField () {
   const [guestConnection, setGuestConnection] = useState()
+  const [aemHost, setAemHost] = useState('')
   let [value, setValue] = useState(null);
   const [model, setModel] = useState(null);
+  const [folders, setFolders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const FOLDERS_QUERY = "/bin/querybuilder.json?" +
+                            "path=/content/dam" +
+                            "&type=sling:Folder" +
+                            "&p.limit=10"
+
+  const fetchRootFolders = async (AEM_HOST, connection) => {
+    const foldersQueryUrl = `${AEM_HOST}${FOLDERS_QUERY}`;       
+    const requestOptions = {
+      headers: {
+        'Authorization': `Bearer ${connection.sharedContext.get("token")}`
+      }
+    };
+    const response = await fetch(foldersQueryUrl, requestOptions)
+    const folderList = (await response.json()).hits?.map(hit => ({
+            path: hit.path,
+            name: hit.name || hit.path.split('/').pop()
+        })) || [];
+
+    console.log("folderList----0000----", folderList);
+
+    return (folderList);
+  };
+
+  const getAemHost = (editorState) => {
+    return editorState.connections.aemconnection.substring(editorState.connections.aemconnection.indexOf('xwalk:') + 6);
+  }
 
   useEffect(() => {
     (async () => {
@@ -22,8 +52,18 @@ export default function EaemDynamicSelectField () {
 
       const model = await connection.host.field.getModel();
       setModel(model);
-
       setValue(await connection.host.field.getValue() || '');
+
+      const editorState = await connection.host.editorState.get();
+
+      if(editorState){
+        const hostValue = getAemHost(editorState);
+        setAemHost(hostValue);
+
+        const folderList = await fetchRootFolders(hostValue, connection);
+        setFolders(folderList);
+        setLoading(false);
+      }
     })()
   }, [])
 
@@ -36,10 +76,15 @@ export default function EaemDynamicSelectField () {
     <Provider theme={defaultTheme} colorScheme='dark'>
       <Content>
         <View padding='size-100'>
-            <ComboBox selectedKey={value} onSelectionChange={handleSelectionChange} label="Root Folder">
-                <Item key="one">One</Item>
-                <Item key="two">Two</Item>
-                <Item key="three">Three</Item>
+            <ComboBox 
+              selectedKey={value} 
+              onSelectionChange={handleSelectionChange} 
+              label="Root Folder"
+              isDisabled={loading}
+            >
+              {folders.map(folder => (
+                <Item key={folder.path}>{folder.name}</Item>
+              ))}
             </ComboBox>
         </View>
       </Content>
