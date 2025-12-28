a5d1adcdc5fbce36061230daffaa62ba56709d4d
nalabotu <sreekanth.choudry@gmail.com>
2025-12-27 21:37:44 -0500

    Update PanelExperienceAemAssetsDuplicateNames.js

--- eaem-app-builder/eaem-assets-show-duplicate-names/src/aem-assets-details-1/web-src/src/components/PanelExperienceAemAssetsDuplicateNames.js
+++ eaem-app-builder/eaem-assets-show-duplicate-names/src/aem-assets-details-1/web-src/src/components/PanelExperienceAemAssetsDuplicateNames.js
@@ -21,6 +21,7 @@ export default function PanelExperienceAemAssetsDuplicateNames() {
   // Fields
   const [guestConnection, setGuestConnection] = useState();
   const [colorScheme, setColorScheme] = useState('light');
+  const [aemToken, setAemToken] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -29,6 +30,14 @@ export default function PanelExperienceAemAssetsDuplicateNames() {
 
       const { colorScheme } = await guestConnection.host.theme.getThemeInfo();
       setColorScheme(colorScheme);
+
+      const aemToken = await guestConnection.sharedContext.get('aemHost');
+      setAemToken(aemToken);
+
+      const imsInfo =  await guestConnection.host.auth.getIMSInfo();
+
+      console.log("imsInfo.imsOrg -> ", imsInfo.imsOrg);
+      console.log("imsInfo.accessToken -> ", imsInfo.accessToken);
     })()
   }, []);
 
@@ -41,7 +50,7 @@ export default function PanelExperienceAemAssetsDuplicateNames() {
     <View backgroundColor="gray-50">
       <View padding="size-300">
         <Text>Please visit <Link href="https://developer.adobe.com/uix/docs/">UI Extensibility documentation</Link> to get started.</Text>
-
+        <Text>AEM Token: {aemToken}</Text>
         <Flex justifyContent="center" marginTop="size-400">
           <ButtonGroup>
             <Button variant="primary" onPress={() => displayToast('neutral', 'Message from the Extension')}>Click me!</Button>
